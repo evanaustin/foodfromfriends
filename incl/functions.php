48bd9441c5019580c01cd20c0cddeec58f711e61
EvanGrinde <evan.grinde@gmail.com>
2017-05-03 16:04:08 -0400

    build functions

---
+++ incl/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+function layer($language, $files) {
+    foreach ($files as $file) {
+        $file .= '.' . $language;
+        
+        if (file_exists(SERVER_ROOT . $file)) {
+            $file_path = PUBLIC_ROOT . $file;
+            
+            switch ($language) {
+                case 'css':
+                    echo '<link rel="stylesheet" href="' . $file_path . '"/>';
+                    break;
+                case 'js':
+                    echo '<script src="' . $file_path . '"></script>';
+                    break;
+            }
+        }
+    }
+}
+
+function quit($message) {
+    $json['error'] = $message;
+    $json['success'] = false;
+    return exit(json_encode($json));
+}
+
+function console_log($data) {
+    echo "<script>console.log('PHP log: " . (is_array($data) ? json_encode($data) : $data) . "');</script>";
+}
+
+?>
