48662cba79bb4513ba09eded7a9efdd8ae226169
EvanGrinde <evan.grinde@gmail.com>
2017-05-03 16:03:59 -0400

    build template

---
+++ routes/template.php
@@ -0,0 +1,27 @@
+<?php if (file_exists($initializer)) include $initializer; ?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $settings['title']; ?></title>
+        <?php layer('css', ['css/app']); ?>
+    </head>
+    <body class="<?php echo $page; ?>">
+        <?php
+        
+        foreach ($body as $part) if (file_exists($part)) include $part;
+        
+        layer('js', [
+            'js/app',
+            'js/ajax',
+            $localScript
+        ]);
+            
+        ?>
+
+        <script>var PUBLIC_ROOT = <?php echo json_encode(PUBLIC_ROOT); ?></script>
+    </body>
+</html>
