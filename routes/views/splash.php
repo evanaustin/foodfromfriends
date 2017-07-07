d969a82f920e656d852ea9b5bf8dc19dfab28df9
EvanGrinde <evan.grinde@gmail.com>
2017-07-06 22:03:04 -0700

    splash view

---
+++ routes/views/splash.php
@@ -0,0 +1,210 @@
+<div class="container-fluid">
+    <section class="hero">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="split-left">
+                    <?php img('logos/artichoke','svg', 'logo'); ?>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="split-right">
+                    <?php img('logos/text','svg', 'business-name'); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="vision">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="split-left align-right">
+                    <h1>
+                        Food the way it used to be
+                    </h1>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="split-right align-left">
+                    <h1>
+                        in a way never done before
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="veggie break">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="split-left">
+                    <div class="veggie-break">
+                        <?php img('icons/veggie_group1_lg','svg'); ?>
+                    </div>
+
+                    <!--div class="hr"></div-->
+                </div>
+            </div>
+            
+            <div class="col-md-6">
+                <div class="split-right">
+                    <div class="veggie-break">
+                        <?php img('icons/veggie_group2_dg','svg'); ?>
+                    </div>
+
+                    <!--div class="hr"></div-->
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="intro">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="split-left">
+                    <p>
+                        Up until 1946, nearly 50% of produce in America was grown in home gardens. Nowadays almost 
+                        all of the food on our plates comes from a handful of industrial farms.
+                    </p>
+                </div>
+            </div>
+            
+            <div class="col-md-6">
+                <div class="split-right">
+                    <p>
+                        We're building a neighbor-to-neighbor marketplace, using the internet to enable 
+                        people to buy and sell food grown from their own porches, backyards, and pastures.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="veggie break">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="split-left">
+                    <?php img('icons/veggie_group2_lg','svg'); ?>
+                </div>
+            </div>
+            
+            <div class="col-md-6">
+                <div class="split-right">
+                    <?php img('icons/veggie_group1_dg','svg'); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="user-types">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="split-left">
+                    <h1>
+                        Locavores
+                    </h1>
+
+                    <h3>
+                        <strong>Locavores</strong> are individuals who buy and eat <br>
+                        food sourced from their own community
+                    </h3>
+
+                    <p>
+                        <strong>Buying on Food From Friends helps them to:</strong>
+                    </p>
+                    
+                    <ul>
+                        <li>improve their local economy</li>
+                        <li>know who their food comes from</li>
+                        <li>lessen their environmental impact</li>
+                    </ul>
+
+                    <p>
+                        <strong>Sign up</strong> to be the first to know when we launch in your area,<br>
+                        so you can search our local offerings of fresh, homegrown food.
+                    </p>
+
+                    <form>
+                        <input type="text" class="form-control" name="email" placeholder="Your email"/>
+                    
+                        <button type="submit" class="btn form-control">
+                            <i class="fa fa-shopping-basket"></i> 
+                            I'm interested in buying
+                        </button>
+                    </form>
+                </div>
+            </div>
+            
+            <div class="col-md-6">
+                <div class="split-right">
+                    <h1 class="user-type">
+                        Local Growers
+                    </h1>
+
+                    <h3>
+                        <strong>Local Growers</strong> are individuals who produce food for their<br>
+                        community at some scale &mdash; small, medium, or large
+                    </h3>
+
+                    <p>
+                        <strong>Selling on Food From Friends helps them to:</strong>
+                    </p>
+
+                    <ul>
+                        <li>earn income from their produce</li>
+                        <li>provide for their neighbors</li>
+                        <li>keep surplus from becoming waste</li>
+                    </ul>
+
+                    <p>
+                        <strong>Sign up</strong> to be the first to know when we launch in your area,<br>
+                        so you can list your food with us and let the hungry people come.
+                    </p>
+
+                    <form>
+                        <input type="text" class="form-control" name="email" placeholder="Your email"/>
+                        
+                        <button type="submit" class="btn form-control">
+                            <i class="fa fa-leaf"></i> 
+                            I'm interested in selling
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="veggie break">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="split-left">
+                    <?php img('icons/veggie_group1_lg','svg'); ?>
+                </div>
+            </div>
+            
+            <div class="col-md-6">
+                <div class="split-right">
+                    <?php img('icons/veggie_group2_dg','svg'); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="split-left">
+                    © 2017. All rights reserved.
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="split-right">
+                    <i class="fa fa-car" aria-hidden="true"></i> 
+                    Made on the road
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
