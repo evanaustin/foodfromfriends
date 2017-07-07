a6971c8eca167f493bce1b9ec40a37c07e1ea6ce
Dustyn Vallies <valliedb@gmail.com>
2017-07-07 19:13:29 -0400

    locavore sign up ajax

---
+++ ajax/splash/locavore_signup.php
@@ -0,0 +1,39 @@
+<?php 
+
+$email = $_POST['email'];
+// $Locavore = new Locavore([
+//     'DB' => $DB
+// ]);
+
+// $Locavore->add('locavores', [
+//     'email' => $email
+// ]);
+
+// $json['error'] = null;
+// $json['success'] = true;
+
+print $email;
+echo $email;
+print 'foo';
+echo 'foo';
+
+
+ // echo json_encode($json);
+
+// $query = "SELECT email FROM locavores WHERE email = '$email'";
+// $result = mysqli_query($db, $query); 
+
+// if (!$result){
+//     $email_verification = "passed";
+// }
+// else{
+// 	$email_verification = "failed";
+// }
+
+// if(!empty($email) && $email_verification=="passed"){
+
+//     insert($table, $email);  
+
+//     }
+
+?>
