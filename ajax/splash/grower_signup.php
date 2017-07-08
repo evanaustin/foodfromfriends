8498a9d33e9fb1ba7fdb98a1eb9c33e759c78a3f
Dustyn Vallies <valliedb@gmail.com>
2017-07-08 00:27:48 -0400

    grower email check ajax

--- ajax/splash/grower_signup.php
+++ ajax/splash/grower_signup.php
@@ -1,4 +1,39 @@
 <?php 
 
+$config = 'config.php';
+while (!file_exists($config)) $config = '../' . $config;
+require $config;
+
+$json['error'] = null;
+$json['success'] = true;
+
+foreach ($_POST as $k => $v) if (isset($v) && !empty($v)) ${$k} = rtrim($v);
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $emailErr = "Invalid email format"; 
+  quit('Could not add grower!');
+}
+
+$Grower = new Grower([
+    'DB' => $DB
+]);
+
+$results = $Grower->exists('growers','email', $email );
+
+if ($results== false){  
+
+    $results = $Grower->add('growers', [
+        'email' => $email
+    ]);
+
+    if (!$results) {
+        quit('Could not add grower!');
+    }
+}
+else{
+      quit('Could not add grower!');
+}
+echo json_encode($json);
+
 
 ?>
