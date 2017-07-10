1c0bd49f8287c3ea4c2da065981be7b2257ed7b4
Dustyn Vallies <valliedb@gmail.com>
2017-07-10 18:54:08 -0400

    locavore signup message ajax

--- ajax/splash/locavore_signup.php
+++ ajax/splash/locavore_signup.php
@@ -11,18 +11,14 @@ foreach ($_POST as $k => $v) if (isset($v) && !empty($v)) ${$k} = rtrim($v);
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     quit('Please enter a valid email.');
-  
 }
-
-
 $Locavore = new Locavore([
     'DB' => $DB
 ]);
 
-
 $results = $Locavore->exists('locavores','email', $email );
 
-if ($results== false){  
+if ($results == false){  
 
     $results = $Locavore->add('locavores', [
         'email' => $email
@@ -31,13 +27,10 @@ if ($results== false){
     if (!$results) {
         quit('Could not add locavore!');
     }
-
 }
 else{
-     
-      quit('This email was already used!');
+      quit('This email was already used.');
 }
-
  echo json_encode($json);
 
 ?>
