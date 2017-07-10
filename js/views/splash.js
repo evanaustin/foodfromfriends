366eef109e3f08857942964ccacec052cf71b705
Dustyn Vallies <valliedb@gmail.com>
2017-07-10 17:33:51 -0400

    JS for splash page error and success message

--- js/views/splash.js
+++ js/views/splash.js
@@ -5,23 +5,27 @@
 	e.preventDefault();
 	console.log('submit');
     console.log($(this).serialize())
-    App.Ajax.post('splash/locavore_signup', $(this).serialize(), function(response){
-		 if (response.success == true){
-			$('#locavore_message').text('Thanks');
-		 }
-	 });	
+    App.Ajax.post('splash/locavore_signup', $(this).serialize(), 
+		function(response){ 
+			$('#locavore_message').text('Thanks for your interest!');
+		},
+		function(response){ 
+			$('#locavore_message').text(response.error);
+		}
+	 );	
   });	
 
 
  $('#grower').on('submit', function(e){
 	e.preventDefault();
 	console.log('submit');
-	App.Ajax.post('splash/grower_signup', $(this).serialize(), function(response){
-		if (response.success == true){
-		$('#grower_message').text('Thanks');
-		 }
-		
-	
-	});	
+	App.Ajax.post('splash/grower_signup', $(this).serialize(), 	
+		function(response){ 
+			$('#grower_message').text('Thanks for your interest!');
+		},
+		function(response){ 
+			$('#grower_message').text(response.error);
+		}
+	);	
   });	
 
