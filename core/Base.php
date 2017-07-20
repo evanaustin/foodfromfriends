7b74a16cb11a2b0dddb6f43d221fc122098d491f
Dustyn Vallies <valliedb@gmail.com>
2017-07-20 11:17:31 -0400

    retrieve function added to Base

--- core/Base.php
+++ core/Base.php
@@ -21,6 +21,28 @@ abstract class Base {
         return (isset($results[0])) ? true : false;
     }
 
+    public function retrieve($table, $field = null, $data = null) {
+        if (!isset($field) && !isset($data)) {
+            $results = $this->DB->run("
+                SELECT * FROM {$table}  
+            ");
+        
+            return (isset($results)) ? $results : false;
+        } else if (isset($field) && isset($data)) {
+            $bind = [
+                'data' => $data
+            ];
+            
+            $results = $this->DB->run("
+                SELECT * FROM {$table} WHERE {$field}=:data 
+            ", $bind);
+            
+            return (isset($results)) ? $results : false;
+        } else {
+            return false;
+        }
+    }
+
     public function add($table, $fields) {
         $results = $this->DB->insert($table, $fields);
         
