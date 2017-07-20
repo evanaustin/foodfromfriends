a8d510eeb1f463b50583bd94f6c287de72b5138a
Dustyn Vallies <valliedb@gmail.com>
2017-07-20 11:23:11 -0400

    built Food_listing class

---
+++ core/Food_listing.php
@@ -0,0 +1,28 @@
+<?php
+ 
+class Food_listing extends Base {
+    
+    protected
+        $class_dependencies,
+        $DB;
+        
+    public
+        $id,
+        $food_subcategory,
+        $price,
+        $description;
+     
+        
+    
+    function __construct($parameters) {
+        $this->class_dependencies = [
+            'DB',
+        ];
+
+        parent::__construct($parameters);
+    
+        if (isset($parameters['id'])) $this->configure_object($parameters['id']);
+    }
+}
+
+?>
