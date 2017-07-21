fc6c91e1e35e15c64f445d13f28483d57c23e03a
EvanGrinde <evan.grinde@gmail.com>
2017-07-20 22:46:04 -0700

    build front architecture

---
+++ routes/architecture/front.php
@@ -0,0 +1,10 @@
+<?php
+
+$body = [
+    'header'    => SERVER_ROOT . 'routes/components/header.php',
+    'view'      => SERVER_ROOT . 'routes/views/' . $path . '.php',
+    'footer'    => SERVER_ROOT . 'routes/components/footer.php',
+    'modal'     => SERVER_ROOT . 'routes/modals/' . $path . '.php',
+];
+
+?>
