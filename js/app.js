b9c83aafc183d048c6cdeeb4b69ed67504caaa2a
EvanGrinde <evan.grinde@gmail.com>
2017-07-20 22:57:31 -0700

    custom parsley validation JS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 // Create the root namespace
 var App = function() {
+	$('form[data-parsley-validate]').parsley({
+		errorClass: 'has-danger',
+		successClass: 'has-success',
+		classHandler: function(ParsleyField) {
+			return ParsleyField.$element.parents('.form-group');
+		},
+		errorsContainer: function(ParsleyField) {
+			return ParsleyField.$element.parents('.form-group');
+		},
+		errorsWrapper: '<div class="form-control-feedback"></div>',
+		errorTemplate: '<span></span>'
+	});
+
+	window.Parsley.on('field:error', function() {
+		this.$element.addClass('form-control-danger');
+	});
+
+	window.Parsley.on('field:success', function() {
+		this.$element.addClass('form-control-success');
+	});
+
 	return {};
 }();
