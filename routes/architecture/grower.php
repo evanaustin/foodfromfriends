c6aa4f39cca125a41aaced6b1b59734df0f62eb5
EvanGrinde <evan.grinde@gmail.com>
2017-07-20 22:46:13 -0700

    build grower architecture

---
+++ routes/architecture/grower.php
@@ -0,0 +1,12 @@
+<?php
+
+$body = [
+    'header'    => 'routes/components/header',
+    'sidebar'   => 'routes/components/grower/sidebar',
+    'subheader' => 'routes/components/grower/subheader',
+    'view'      => 'routes/views/' . $Routing->path,
+    'footer'    => 'routes/components/footer',
+    'modal'     => 'routes/modals/' . $Routing->path,
+];
+
+?>
