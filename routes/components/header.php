3645a3294054cbc6bfffba1351d52673533fbaeb
EvanGrinde <evan.grinde@gmail.com>
2017-07-20 22:59:25 -0700

    build navbar

--- routes/components/header.php
+++ routes/components/header.php
@@ -1,3 +1,21 @@
-<div id="header">
-    <?php img('logos/artichoke_white_no-accents', 'svg'); ?>
-</div>
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <a class="navbar-brand" href="#">
+        <?php svg('logos/thin'); ?>
+    </a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#sign-up-modal">Sign up</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#log-in-modal">Log in</a>
+            </li>
+        </ul>
+    </div>
+</nav>
