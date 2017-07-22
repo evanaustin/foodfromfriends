55ebcf5bb8c7b28f4137ef61d7e7a63b65957783
EvanGrinde <evan.grinde@gmail.com>
2017-07-22 15:35:58 -0700

    show different header links whether or not logged in

--- routes/components/header.php
+++ routes/components/header.php
@@ -9,13 +9,28 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="modal" data-target="#sign-up-modal">Sign up</a>
-            </li>
 
-            <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="modal" data-target="#log-in-modal">Log in</a>
-            </li>
+            <?php if (!$LOGGED_IN) { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#sign-up-modal">Sign up</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#log-in-modal">Log in</a>
+                </li>
+            <?php } else { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Grow</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Shop</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Messages</a>
+                </li>
+            <?php } ?>
         </ul>
     </div>
 </nav>
