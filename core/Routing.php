55dee822bc9ea169530e86e1024297f22494d8eb
EvanGrinde <evan.grinde@gmail.com>
2017-07-22 15:27:27 -0700

    add some path properties to Routing

--- core/Routing.php
+++ core/Routing.php
@@ -3,16 +3,25 @@
 class Routing {
     public
         $path,
-        $page,
-        $template;
+        $fullpage,
+        $template,
+        $section,
+        $subsection,
+        $page;
 
     function __construct($parameters) {
         $this->path = (empty($parameters['path']) ? $parameters['landing'] : $parameters['path']);
-        $this->page = str_replace('/', '-', $this->path);
+        $this->fullpage = str_replace('/', '-', $this->path);
 
         $exp_path  = explode('/', $this->path);
 
         $this->template = ($this->path == 'home' || !in_array($exp_path[0], $parameters['backside'])) ? 'front' : $exp_path[0];
+
+        if ($this->template != 'front') {
+            $this->section = (isset($exp_path[1])) ? $exp_path[1] : null;
+            $this->subsection = (isset($exp_path[2])) ? $exp_path[2] : null;
+            $this->page = (isset($exp_path[3])) ? $exp_path[3] : null;
+        }
     }
 }
 
