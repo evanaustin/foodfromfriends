3e6ec43334e9c1d4f2d69649aefbea5cc6d533ca
EvanGrinde <evan.grinde@gmail.com>
2017-07-22 15:36:44 -0700

    make grower subheader dynamic

--- routes/components/grower/subheader.php
+++ routes/components/grower/subheader.php
@@ -1,4 +1,25 @@
-<div class="side-right">
-    <div class="subheader">
-        Foo
-    </div>
+<!-- cont div.container-fluid -->
+    <!-- cont div.row -->
+        <main class="col-sm-9 col-md-10">
+            <div class="subheader">
+                <ul class="nav nav-fill">
+                    <?php foreach ([
+                        'dashboard',
+                        'stats',
+                        'profile',
+                        'account',
+                    ] as $title) { ?>
+                        <li class="nav-item">
+                            <a 
+                                href="<?php echo PUBLIC_ROOT . $Routing->template . '/' . $title; ?>" 
+                                class="nav-link <?php if ($Routing->section == $title) echo 'active'; ?>"
+                            >
+                                <?php echo ucfirst($title) ?>
+                            </a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        <!-- cont main -->
+    <!-- cont div.row -->
+<!-- cont div.container-fluid -->
