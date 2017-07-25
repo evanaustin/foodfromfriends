0ff5b77532cbcade6d0c52181e63496164569fab
EvanGrinde <evan.grinde@gmail.com>
2017-07-24 18:08:04 -0700

    add user dropdown to header

--- routes/components/header.php
+++ routes/components/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<nav id="header" class="navbar navbar-toggleable-md navbar-light bg-faded">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -29,6 +29,16 @@
 
                 <li class="nav-item">
                     <a class="nav-link" href="#">Messages</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <div class="nav-link dropdown-toggle profile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></div>
+                
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Edit profile</a>
+                        <a class="dropdown-item" href="#">Account settings</a>
+                        <a id="log-out" class="dropdown-item" href="#">Log out</a>
+                    </div>
                 </li>
             <?php } ?>
         </ul>
