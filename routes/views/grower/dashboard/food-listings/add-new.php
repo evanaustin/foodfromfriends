540d7aefd5d336ae9b741327ace40f2269fe19d7
EvanGrinde <evan.grinde@gmail.com>
2017-07-25 11:03:40 -0700

    refactor add new listing view

--- routes/views/grower/dashboard/food-listings/add-new.php
+++ routes/views/grower/dashboard/food-listings/add-new.php
@@ -2,100 +2,110 @@
     <!-- cont div.row -->
         <!-- cont main -->
             <div class="container">
-                <h3> Grower Dashboard</h3><br>
-                <!--<form id="add-listing" class= "food_listing_form" action="upload.php" method="post" enctype="multipart/form-data">-->
-                <form id="add-listing" class= "food_listing_form">
-                    <h6>What do you have?</h6>
-                    <?php echo "<select id='food_category' name= 'food_category'>";
-                        foreach ($categories as $category){           
+                <h4 class="title">Add a new food listing</h4>
+                <hr>
 
-                            echo "<option value=$category[id]>$category[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?> <br><br>
+                <div class="alert"></div>
 
-                    <?php echo "<select id='veg' class='food_subcategory' name= 'food_subcategory_id'>";
-                        foreach ($subcategories_veg as $subcategory_veg){           
+                <form id="add-listing" class="food_listing_form" data-parsley-validate>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="food-categories">What kind of food do you have?</label>
+                        </div>
 
-                            echo "<option value=$subcategory_veg[id]>$subcategory_veg[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?> 
-                    <?php echo "<select id='fruit' class='food_subcategory' name= 'food_subcategory_id'>";
-                        foreach ($subcategories_fruit as $subcategory_fruit){           
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <select id="food-categories" name="food-category" class="custom-select form-control" data-parsley-trigger required>
+                                    <option selected disabled>Select a food category</option>
 
-                            echo "<option value=$subcategory_fruit[id]>$subcategory_fruit[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?> 
-                    <?php echo "<select id='egg' class='food_subcategory' name= 'food_subcategory_id'>";
-                        foreach ($subcategories_egg as $subcategory_egg){           
+                                    <?php foreach($food_categories as $food_category) { ?>
+                                        <option value="<?php echo $food_category['id'] ?>"><?php echo ucfirst($food_category['title']); ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
 
-                            echo "<option value=$subcategory_egg[id]>$subcategory_egg[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?>    
-                    <?php echo "<select id='dairy' class='food_subcategory' name= 'food_subcategory_id'>";
-                        foreach ($subcategories_dairy as $subcategory_dairy){           
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <select id="food-subcategories" name="food-subcategory" class="custom-select form-control" data-parsley-trigger disabled required>
+                                    <option selected disabled>Select a food subcategory</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
 
-                            echo "<option value=$subcategory_dairy[id]>$subcategory_dairy[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?> 
-                    <?php echo "<select id='meat' class='food_subcategory' name= 'food_subcategory_id'>";
-                        foreach ($subcategories_meat as $subcategory_meat){           
+                    <div id="other-option" class="form-group">
+                        <label for="other-subcategory">Can't find the food you're trying to list?</label>
+                        <input id="other-subcategory" type="text" name="other" class="form-control" placeholder="Add your food type">
+                    </div>
 
-                            echo "<option value=$subcategory_meat[id]>$subcategory_meat[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?> 
-                    <?php echo "<select id='sea' class='food_subcategory' name= 'food_subcategory_id'>";
-                        foreach ($subcategories_sea as $subcategory_sea){           
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label for="price">Listing price</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">$</div>
+                                    <input id="price" type="number" name="price" class="form-control" placeholder="Enter a price for your food">
+                                </div>
+                            </div>
+                        </div>
 
-                            echo "<option value=$subcategory_sea[id]>$subcategory_sea[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?> 
-                    <?php echo "<select id='bev' class='food_subcategory' name= 'food_subcategory_id'>";
-                        foreach ($subcategories_bev as $subcategory_bev){           
+                        <div class="col-md-9">
+                            <div class="form-group">
+                                <label>Upload a listing image</label>
+                                
+                                <div id="listing-image">
+                                    <label class="custom-file">
+                                        <input type="file" name="listing-image" class="custom-file-input form-control">
+                                        <span class="custom-file-control"></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                            echo "<option value=$subcategory_bev[id]>$subcategory_bev[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?> 
-                    <?php echo "<select id='herb' class='food_subcategory' name= 'food_subcategory_id'>";
-                        foreach ($subcategories_herb as $subcategory_herb){           
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="quantity">Quantity</label>
+                                <input id="quantity" type="number" name="quantity" class="form-control" placeholder="Enter how many you have in stock">
+                            </div>
+                        </div>
 
-                            echo "<option value=$subcategory_herb[id]>$subcategory_herb[title]</option>"; 
-                        }
-                        echo"</select>";
-                    ?> 
-                    <br><br>
-                    <!--Add New Subcategory-->
-                    <a id="add_new_subcategory" href="#">Can't find the food you are trying to list?</a><br> 
-                    <input id='new_subcategory' type='text' name='new_subcategory' placeholder='Add your food.'/><br>
-                    <!--Upload File-->
-                    <h6> Select image to upload:</h6> <br>
-                    <input type="file" name="food_listing_image" id="fileToUpload"><br><br>
-                    <!--Set Price-->
-                    <h6>How much are you selling them for?</h6>
-                    $ <input type="number" name="price" min="0.01" step="0.01" max="100" value="1.50" /><br><br><br>
-                    <!--Add Description-->
-                    <textarea type="text" class="form-control" name="description" placeholder =" Add a description">Add a description of your home grown food!</textarea><br>
-                    <!--Set Stock-->
-                    <h6>How many do you have?</h6>
-                    <input type="number" name="stock" min="1" step="1" max="10000" value="1" /> <br><br>
-                    <!--Is Avaible?-->
-                    <h6>Is this listing avaible for neighbors to request?</h6>
-                    <input id="available" type="radio" name="is_available" value="0" checked /> Oh yea! <br>
-                    <input id="unavailable" type="radio" name="is_available" value="1" /> Nope.<br>
-                    <!--Submit-->
-                    <button type="submit" class="btn form-control">
-                        <i class="fa fa-shopping-basket"></i> 
-                        Submit
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Availability</label>
+                                
+                                <div class="radio-box">
+                                    <label class="custom-control custom-radio">
+                                        <input id="radio1" name="radio" type="radio" class="custom-control-input">
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">Available</span>
+                                    </label>
+
+                                    <label class="custom-control custom-radio">
+                                        <input id="radio2" name="radio" type="radio" class="custom-control-input">
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">Unavailable</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description">Listing description</label>
+                        <textarea type="text" name="description" class="form-control" rows="4" placeholder="Write a description of your homegrown food"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-block">
+                        Create listing
                     </button>
-                    <p class="listing-message"></p>
-                </form>  
+                </form>
             </div> <!-- end main -->
         </div> <!-- end div.row -->
     </div> <!-- end div.container-fluid -->
+
+    <script>
+        var food_subcategories = <?php echo json_encode($food_subcategories); ?>;
+    </script>
