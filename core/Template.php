ab99006bc08b4e3e650f28e1c0730586b054cefe
EvanGrinde <evan.grinde@gmail.com>
2017-07-24 18:05:29 -0700

    set JS scripts in Template

--- core/Template.php
+++ core/Template.php
@@ -5,18 +5,29 @@ class Template {
         $styles,
         $initializer,
         $architecture,
-        $script;
+        $scripts;
     
     function __construct($Routing) {
         $construct = [
-            'styles'       => 'css/' . $Routing->template,
-            'initializer'  => 'routes/initializers/' . $Routing->path,
-            'architecture' => 'routes/architecture/' . $Routing->template,
-            'script'       => 'js/views/' . $Routing->path
+            'styles'        => 'css/' . $Routing->template,
+            'initializer'   => 'routes/initializers/' . $Routing->path,
+            'architecture'  => 'routes/architecture/' . $Routing->template,
+            'scripts'       => [
+                'js/views/' . $Routing->path
+            ]
         ];
 
         foreach ($construct as $k => $v) {
             $this->{$k} = $v;
+        }
+
+        $this->set_scripts($Routing->template);
+    }
+
+    function set_scripts($template) {
+        if ($template == 'front') {
+            $this->scripts['sign-up'] = 'js/modals/sign-up';
+            $this->scripts['log-in'] = 'js/modals/log-in';
         }
     }
 }
