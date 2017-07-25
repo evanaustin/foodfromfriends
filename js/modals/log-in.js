65cc195bfe215ff9fd3a9a27d4f101968911292e
EvanGrinde <evan.grinde@gmail.com>
2017-07-24 18:21:38 -0700

    fix log in JS

--- js/modals/log-in.js
+++ js/modals/log-in.js
@@ -3,11 +3,11 @@ $('#log-in').on('submit', function(e) {
     $form = $(this);
     
     App.Ajax.post('account/log_in', $form.serialize(), 
-		function(response) {
-            window.location.replace(PUBLIC_ROOT + '/dashboard');
-		},
-		function(response) {
+      function(response) {
+            window.location.replace(PUBLIC_ROOT + 'dashboard');
+      },
+      function(response) {
             $form.siblings('div.alert').addClass('alert-danger').html('<i class="fa fa-exclamation-triangle"></i> ' + response.error).show();
-		}
+      }
     ); 
 });	
