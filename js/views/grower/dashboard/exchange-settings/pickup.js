0fb2ddf0b3095bcc1bf1f36b9257eea961e6d4cb
Dustyn Vallies <valliedb@gmail.com>
2017-07-27 20:35:32 -0400

    deleted grower folder

--- js/views/grower/dashboard/exchange-settings/pickup.js
+++
@@ -1,9 +0,0 @@
-$('input[name="pickup-setting"]').on('change', function() {
-    if ($(this).val() == 1) {
-        $('#pickup-option').show();
-        $('#pickup-description-and-time').show();
-    } else {
-       $('form > div:not(#pickup-setting)').hide();
-    }
-});
-
