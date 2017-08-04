f065e15c0c7502a8da75531f6617573f371b7456
EvanGrinde <evan.grinde@gmail.com>
2017-08-03 18:43:07 -0600

    use cards for overview

--- routes/views/dashboard/food-listings/overview.php
+++ routes/views/dashboard/food-listings/overview.php
@@ -1,26 +1,54 @@
 <!-- cont div.container-fluid -->
     <!-- cont div.row -->
         <!-- cont main -->
+            <div class="titlebar">
+                <div class="container">
+                    Your food listings
+                </div>
+            </div>
+            
             <div class="container">
-                <h4 class="title">Your food listings</h4>
+                <?php
+                
+                $i = 1;
 
-                <hr>
+                foreach($listings as $listing) {
+                    if ($i % 3 == 1) {
+                        echo '<div class="card-deck">';
+                    }
 
-                <div class="row">
-                <?php foreach($listings as $listing) { ?>
-                    <div class="col-md-4">
-                        <div class="card">
-                            <img class="card-img-top" src="" alt="Card image cap">
+                    ?>
 
-                            <div class="card-block">
-                                <h4 class="card-title"><?php echo ucfirst((empty($listing['other_subcategory']) ? ($listing['subcategory_title']) : $listing['other_subcategory'])); ?></h4>
-                                <p class="card-text"><?php echo $listing['description']; ?></p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
+                    <div class="card">
+                        <?php
+                        
+                        if (!empty($listing['filename'])) {
+                            img('user/' . $listing['filename'], $listing['ext'], 'S3', 'card-img-top');
+                        } else {
+                            img('placeholders/default-thumbnail', 'jpg', 'local', 'card-img-top');
+                        }
+
+                        ?>
+
+                        <div class="card-block">
+                            <h4 class="card-title"><?php echo ucfirst((empty($listing['other_subcategory']) ? ($listing['subcategory_title']) : $listing['other_subcategory'])); ?></h4>
+                            <h6 class="card-subtitle mb-2 text-muted">$<?php echo number_format($listing['price'] / 100, 2); ?></h6>
+                            <p class="card-text"><?php echo $listing['quantity'] . ' in stock • ' . (($listing['is_available']) ? 'Available' : 'Unavailable'); ?></p>
+                            <a href="<?php echo PUBLIC_ROOT . 'dashboard/food-listings/view?id=' . $listing['id']; ?>" class="btn btn-primary">Edit listing</a>
                         </div>
                     </div>
-                <?php } ?>
-                </div>
+                    
+                    <?php
+
+                    if ($i % 3 == 0) {
+                        echo '</div>';
+                    }
+
+                    $i++;
+                }
+
+                ?>
+                    
             </div>
         <!-- cont main -->
     <!-- cont div.row -->
