fdb4430a099a595b5bcc7abbc5d84b8c883af8b2
EvanGrinde <evan.grinde@gmail.com>
2017-08-17 17:29:51 -0600

    build remove image AJAX

---
+++ ajax/dashboard/food-listings/remove-image.php
@@ -0,0 +1,30 @@
+<?php 
+
+$config = 'config.php';
+while (!file_exists($config)) $config = '../' . $config;
+require $config;
+
+$json['error'] = null;
+$json['success'] = true;
+
+$FoodListing = new FoodListing([
+    'DB' => $DB,
+    'S3' => $S3,
+    'id' => $_POST['listing_id']
+]);
+
+if (!empty($FoodListing->filename)) {
+    $record_removed = $FoodListing->delete('filename', $FoodListing->filename, 'food_listing_images');
+    
+    if (!$record_removed) quit('Could not edit image record');
+    
+    $img_removed = $S3->delete_objects([
+        'user/' . $FoodListing->filename . '.' . $FoodListing->ext
+    ]);
+} else {
+    quit('There was no image to remove');
+}
+
+echo json_encode($json);
+
+?>  
