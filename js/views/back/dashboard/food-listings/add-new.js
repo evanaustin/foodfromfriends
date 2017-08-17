ecc0a927119f21ea34384fbe624808db8a0c0653
EvanGrinde <evan.grinde@gmail.com>
2017-08-17 17:31:17 -0600

    refactor add new JS

--- js/views/back/dashboard/food-listings/add-new.js
+++ js/views/back/dashboard/food-listings/add-new.js
@@ -1,125 +1,182 @@
-    image = { 
-        uploadDisabled: [false, false, false, false],
+image = { 
+    uploadDisabled: [false, false, false, false],
 
-        source: [],
+    source: [],
 
-        crop: [],
+    crop: [],
 
-        files: [],
+    files: [],
 
-        frame: {
-            w: $('img.file').width(),
-            h: Math.floor($('img.file').width() * 1080 / 1240)
-            // h: Math.floor($('img.file').width() * 824 / 1240)
-        },
+     frame: {
+        w: Math.floor($('div.image-box').outerWidth()),
+        h: Math.floor($('div.image-box').outerHeight())
+    }, 
 
-        // Hide image upload inputs in incompatible browsers
-        /* init: function() {
-            if (!window.File || !window.FileReader || !window.FileList || !window.Blob || !window.FormData) {
-                $('#images-box').hide();
-            }
-        }, */
+    init: function() {
+        $('div.image-box').height(Math.floor($('div.image-box').width() * 540 / 630));
+    },
 
-        selectFile: function(el) {
-            el.find('input[type=file]').trigger('click');
-        },
+    selectFile: function(el) {
+        el.find('input[type=file]').trigger('click');
+    },
 
-        onceSelected: function(el, e) {
-            var file = e.target.files[0];
-            var self = this;
+    onceSelected: function(el, e) {
+        // reset frame height
+        this.frame.h = Math.floor($('div.image-box').outerHeight());
+        
+        // clear slide-over image class
+        $('div.image-box').removeClass('slide-over').unbind('mouseover').unbind('mouseout');
 
-            // App.Util.hideMsg();
+        // only one image so key is always 0
+        var key = 0;
 
-            if (file.type != 'image/png' && file.type != 'image/jpeg') {
-                App.Util.msg('Please select a JPG or PNG image', 'danger');
-                return;
-            }
+        var file = e.target.files[key];
+        var self = this;
 
-            if (file.size > 3 * 1024 * 1024) {
-                App.Util.msg('Please select a file less than 3MB in size', 'danger');
-                return;
-            }
+        // App.Util.hideMsg();
 
-            var $img = $('div.image-box').find('img.file');
-
-            console.log($img);
-
-            // Show the loading image
-            // It's 1px too tall, so we fix the height here
-            // TODO: get a gif editing program and edit the gif
-            /* var h = $img.height();
-            $img.attr({
-                src: '/images/loading-box.gif',
-                height: h
-            }); */
-
-            // Load the file into the cropping window
-            var reader = new FileReader();
-            reader.onload = (function (f) {
-                return function(e) {
-                    var img = new Image;
-                    img.src = e.target.result;
-
-                    img.onload = function() {
-                        var $img = $('div.image-box').find('img.file');
-
-                        // Show preview
-                        $img.removeAttr('height').attr('src', img.src);
-
-                        // Launch crop script
-                        $img.cropbox({
-                            width: self.frame.w,
-                            height: self.frame.h,
-                            maxZoom: 10.0,
-                            zoom: 20
-                        }).on('cropbox', function(e, data) {
-                            self.crop[0] = {
-                                x: Math.abs(data.cropX),
-                                y: Math.abs(data.cropY),
-                                w: Math.abs(data.cropW),
-                                h: Math.abs(data.cropH)
-                            };
-                        });
-
-                        // Add delete button to controls menu
-                        // $img.next('div.cropControls').prepend('<button class="removeImg" data-key="' + key + '"><i class="fa fa-trash-o"></i></button>');
-
-                        self.source = {
-                            w: img.width,
-                            h: img.height
-                        };
-
-                        // Disable click-to-upload
-                        self.uploadDisabled[0] = true;
-
-                        self.files[0] = file;
-                    }
-                };
-            })(file);
-            reader.readAsDataURL(file);
-        },
-
-        getCropData: function() {
-            var data = [];
-            var self = this;
-
-            $('div.image-box').find('img.file').each(function() {
-                if (self.uploadDisabled[0] === false) {
-                    return true;    // continue
-                }
-
-                data.push({
-                    source: self.source[0],
-                    crop: self.crop[0]
-                });
-            });
-
-            return {
-                images: data,
-                frame: self.frame
-            };
+        if (file.type != 'image/png' && file.type != 'image/jpeg') {
+            App.Util.msg('Please select a JPG or PNG image', 'danger');
+            return;
         }
+
+        if (file.size > 3 * 1024 * 1024) {
+            App.Util.msg('Please select a file less than 3MB in size', 'danger');
+            return;
+        }
+
+        var $img = $('div.image-box').find('img.file');
+
+        // Load the file into the cropping window
+        var reader = new FileReader();
+        reader.onload = (function (f) {
+            return function(e) {
+                var img = new Image;
+                img.src = e.target.result;
+
+                img.onload = function() {
+                    var $img = $('div.image-box').find('img.file');
+
+                    // Show & animate preview
+                    $img.removeAttr('height').attr('src', img.src);
+                    App.Util.animation($('div.image-box img.file'), 'pulse');
+
+                    // Launch crop script
+                    $img.cropbox({
+                        width: self.frame.w,
+                        height: self.frame.h,
+                        maxZoom: 10.0,
+                        zoom: 20
+                    }).on('cropbox', function(e, data) {
+                        self.crop[key] = {
+                            x: Math.abs(data.cropX),
+                            y: Math.abs(data.cropY),
+                            w: Math.abs(data.cropW),
+                            h: Math.abs(data.cropH)
+                        };
+                    });
+
+                    // Show delete button
+                    App.Util.animation($('a.remove-image'), 'bounceIn', 'in');
+
+                    self.source[key] = {
+                        w: img.width,
+                        h: img.height
+                    };
+
+                    // Disable click-to-upload
+                    self.uploadDisabled[key] = true;
+
+                    self.files[key] = file;
+                }
+            };
+        })(file);
+
+        reader.readAsDataURL(file);
+    },
+
+    getCropData: function() {
+        var data = [];
+        var self = this;
+
+        // only one image so key is always 0
+        var key = 0;
+
+        $('div.image-box').find('img.file').each(function() {
+            if (self.uploadDisabled[key] === false) {
+                return true;    // continue
+            }
+
+            data.push({
+                source: self.source[key],
+                crop: self.crop[key],
+                key: key
+            });
+        });
+
+        return {
+            images: data,
+            frame: self.frame
+        };
+    },
+
+    discard: function() {
+        // only one image so key is always 0
+        var key = 0;
+
+        this.uploadDisabled[key] = false;
+        this.source.splice(key, 1);
+        this.crop.splice(key, 1);
+        this.files.splice(key, 1);
+
+        var $img = $('div.image-box').find('img.file');
+
+        if (typeof $img.data('cropbox') != 'undefined') {
+            $img.data('cropbox').remove();
+        }
+
+        // reset
+        $img.attr('src', PUBLIC_ROOT + '/media/placeholders/default-thumbnail.jpg');
+        $('input[type="file"]').val('');
+
+        // hide discard icon tooltip
+        $('a.remove-image').tooltip('hide');
+
+        // animate discard departure
+        App.Util.animation($('a.remove-image'), 'bounceOut', 'out');
     }
+}
+
+
+// Initialize imaging
+image.init();
+
+
+// Upload image
+$('div.image-box').on('click', function(e) {
+    // only one image so key is always 0
+    var key = 0;
+    
+    if (image.uploadDisabled[0] === false) {
+        image.selectFile($(this));
+    }
+});
+
+$('div.image-box input[type=file]').on('click', function(e) {
+    e.stopImmediatePropagation();
+});
+
+$('div.image-box input[type=file]').on('change', function(e) {
+    var success = image.onceSelected($(this), e);
+});
+
+
+// Discard uploaded image
+$('a.remove-image').on('click', function(e) {
+    e.preventDefault();
+    image.discard();
+});
+
 
 // Re-populate subcategory select menu
 $('#food-categories').on('change', function() {
@@ -137,17 +194,23 @@ $('#food-categories').on('change', function() {
     $('#food-subcategories').append('<option value="0">Other</option>');
 });
 
+
 // Display other input field
 $('#food-subcategories').on('change', function() {
     if ($(this).val() == 0) {
-        $('#other-option').show(function() {
+        if ($('#other-option').is(':not(:visible)')) {
+            App.Util.animation($('#other-option'), 'flipInX', 'in');
             $('#other-subcategory').focus();
-        });
+        }
     } else {
-        $('#other-option').hide();
+        if ($('#other-option').is(':visible')) {
+            App.Util.animation($('#other-option'), 'fadeOut', 'out');
+        }
     }
 });
 
+
+// Check if other already exists as a subcategory
 $('#other-subcategory').on('keyup change', function() {
     var other = $(this);
 
@@ -155,7 +218,7 @@ $('#other-subcategory').on('keyup change', function() {
         if (el.text.charAt(0).toUpperCase() + el.text.slice(1) == other.val().charAt(0).toUpperCase() + other.val().slice(1)) {
             el.setAttribute('selected', true);
             other.val('');
-            $('#other-option').hide();
+            App.Util.animation($('#other-option'), 'fadeOut', 'out');
             toastr.info(el.text + ' already exists as an option');
 
             return false;
@@ -165,6 +228,8 @@ $('#other-subcategory').on('keyup change', function() {
     });
 });
 
+
+// Update availability as quantity is changed
 $('#quantity').on('keyup change', function() {
     if ($(this).val() == 0) {
         $('#available').prop('checked', false);
@@ -175,49 +240,59 @@ $('#quantity').on('keyup change', function() {
     } 
 });
 
-$('div.image-box').on('click', function(e) {
-    if (image.uploadDisabled[0] === false) {
-        image.selectFile($(this));
-    }
-});
 
-$('div.image-box input[type=file]').on('click', function(e) {
-    e.stopImmediatePropagation();
-});
-
-$('div.image-box input[type=file]').on('change', function(e) {
-    var success = image.onceSelected($(this), e);
-    
-    if (!success) {
-
-    }
-});
-
+// Add listing
 $('#add-listing').on('submit', function(e) {
-	e.preventDefault();
-    
+    e.preventDefault();
+
+    App.Util.hideMsg();
+
     $form = $(this);
 
     if (window.FormData){
-        data = new FormData($form[0]);
+        formdata = new FormData($form[0]);
+        
+        if (image.files.length > 0) {
+            $.each(image.files, function(k, v) {
+                formdata.append('img' + k, v);
+            });
+    
+            formdata.append('images', JSON.stringify(image.getCropData()));
+        }
+
+        data = formdata;
     } else {
         data = $form.serialize();
     }
 
     if ($form.parsley().isValid()) {
+        App.Util.loading();
+        
         App.Ajax.postFiles('dashboard/food-listings/add-new', data, 
             function(response) {
-                toastr.success('Your listing has been created!');
-                
+                App.Util.msg('Your listing has been created! Click <strong><a href="' + PUBLIC_ROOT + 'dashboard/food-listings/edit?id=' + response.id + '">here</a></strong> to edit it, or add another new listing below.', 'success');
+                App.Util.animation($('button[type="submit"]'), 'bounce');
+                App.Util.finishedLoading();
+
                 // clear form
                 $form.each(function() {
                     this.reset();
                     $('div.form-group').removeClass('has-success');
                     $('input, select').removeClass('form-control-success');
                 });
+
+                // reset subcategories
+                $('#food-subcategories').prop('disabled', true).empty().focus().append('<option selected disabled>Select a food subcategory</option>');
+
+                // hide other option
+                $('#other-option').fadeOut();
+
+                // clear image
+                image.discard();
             },
             function(response) {
-                $form.siblings('div.alert').addClass('alert-danger').html('<i class="fa fa-exclamation-triangle"></i> ' + response.error).show();
+                App.Util.msg(response.error, 'danger');
+                App.Util.finishedLoading();
             }
         );
     }
