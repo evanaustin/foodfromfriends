f75c9b9ee054ab95d72ce340530cf964c87eb7bb
EvanGrinde <evan.grinde@gmail.com>
2017-08-19 15:15:12 -0600

    refactor delivery view

--- routes/views/dashboard/exchange-settings/delivery.php
+++ routes/views/dashboard/exchange-settings/delivery.php
@@ -1,106 +1,155 @@
 <!-- cont div.container-fluid -->
     <!-- cont div.row -->
         <!-- cont main -->
-            <div class="container">
-                <h4 class="title">Add your delivery preferences</h4>
-                <hr>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="alert"></div>
-                    <form id="save-delivery" class="delivery-form" data-parsley-validate>
-                        <div id="delivery-setting">
-                            <div class="form-group">
-                                <label>Want to make extra money by offering delivery?</label>
-                                
-                                <div class="radio-box">
-                                    <label class="custom-control custom-radio">
-                                        <input id="delivery-yes" name="is-offered" type="radio" value="1" class="custom-control-input" <?php if($details['is_offered'] == 1){echo 'checked';}?>>
-                                        <span class="custom-control-indicator"></span>
-                                        <span class="custom-control-description">Yes</span>
-                                    </label>
-
-                                    <label class="custom-control custom-radio">
-                                        <input id="delivery-no" name="is-offered" type="radio" value="0" class="custom-control-input"  <?php if($details['is_offered'] == 0){echo 'checked';}?> >
-                                        <span class="custom-control-indicator"></span>
-                                        <span class="custom-control-description">No</span>
-                                    </label>
-                                </div>
-                            </div>
+            <div class="main container animated fadeIn">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="page-title">
+                            Set your delivery preferencs
                         </div>
 
-                        <div id="distance-and-free-delivery-option">
-                            <div class="form-group">
-                                <label for="distance">Distance to buyer (miles)</label>
-                                <input id="distance" type="number" name="distance" class="form-control " placeholder="Enter how many miles you would be willing to travel (one way)" value="<?php echo (!empty($details) ? ($details['distance']) : '' ); ?>" min="0" data-parsley-min="0" data-parsley-trigger disabled>
-                            </div>
-                            <div class="form-group">  
-                                <label>Want to offer free delivery?</label>
-                                
-                                <div class="radio-box">
-                                    <label class="custom-control custom-radio">
-                                        <input id="free-delivery" name="free-delivery" type="radio" value="1" class="custom-control-input"  <?php if($details['free_delivery'] == 1){echo 'checked';} ?> data-parsley-trigger disabled >
-                                        <span class="custom-control-indicator"></span>
-                                        <span class="custom-control-description">Yes</span>
-                                    </label>
-
-                                    <label class="custom-control custom-radio">
-                                        <input id="no-free-delivery" name="free-delivery" type="radio"  value="2" class="custom-control-input" <?php if($details['free_delivery'] == 2){echo 'checked';}?>  data-parsley-trigger disabled >
-                                        <span class="custom-control-indicator"></span>
-                                        <span class="custom-control-description">No</span>
-                                    </label>
-                                    <label class="custom-control custom-radio">
-                                        <input id="conditional-free-delivery" name="free-delivery" type="radio"  value="conditional" class="custom-control-input" <?php if($details['free_delivery'] == 'conditional'){echo 'checked';}?>  data-parsley-trigger disabled >
-                                        <span class="custom-control-indicator"></span>
-                                        <span class="custom-control-description">Only under a certain distance</span>
-                                    </label>
-                                </div>
-                            </div>
-                        </div><!--close question-one-->
-
-                        <div id="conditional-free-delivery-option">
-                            <div class="form-group">
-                                <label for="free-distance">Free delivery distance (Miles)</label>
-                                <input id="free-distance" type="number" name="free-miles" class="form-control " placeholder="Enter how many miles you would be willing to travel for free (one way)" value="<?php echo (!empty($details) ? ($details['free_miles']) : '' ); ?>"   min="0.1" data-parsley-min="0.1" step="0.1" data-parsley-trigger disabled>
-                            </div>
-                        </div> <!--close out class conditional free delivery -->
-
-                        <div id="choose-delivery-fee-option">
-                            <label>Have a set delivery fee or have a fee for each mile traveled?</label>
-
-                            <div class="radio-box">
-                                <label class="custom-control custom-radio">
-                                    <input id="per-mile-fee"  name="pricing-rate" type="radio"  value="mile" class="custom-control-input" data-parsley-trigger disabled <?php if($details['pricing_rate'] == 'mile'){echo 'checked';}?>>
-                                    <span class="custom-control-indicator"></span>
-                                    <span class="custom-control-description">Price per mile</span>
-                                </label>
-
-                                <label class="custom-control custom-radio">
-                                    <input id="set-fee"  name="pricing-rate" type="radio" value="flat" class="custom-control-input"data-parsley-trigger disabled <?php if($details['pricing_rate'] == 'flat'){echo 'checked';}?>>
-                                    <span class="custom-control-indicator"></span>
-                                    <span class="custom-control-description">Set Price</span>
-                                </label>
-                            </div>
-                        </div> <!--close choose delivery --> 
-                        <div id="per-mile-option">
-                            <label for="fee"> Delivery fee per mile</label>  
+                        <div class="page-description text-muted small">
+                            Thinking about making an extra buck by offering delivery to your customers? With all our customizable settings, you can create a delivery policy that suits you perfectly.
                         </div>
-                        <div id="set-fee-option">
-                            <label for="setprice"> Set delivery fee</label>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="controls">
+                            <button type="submit" form="save-delivery" class="btn btn-primary">
+                                <i class="pre fa fa-floppy-o"></i>
+                                Save changes
+                                <i class="post fa fa-gear loading-icon"></i>
+                            </button>
                         </div>
-                        <div id='fee'>
-                            <div class="form-group">
-                                <div class="input-group">
-                                    <div class="input-group-addon">$</div>
-                                    <input type="number" name="fee" class="form-control" placeholder="Enter a delivery fee" value="<?php echo (!empty($details) ? ($details['fee']) : '' ); ?>"   min="0.01" data-parsley-min="0.01" step="0.01"data-parsley-trigger disabled>
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-block ">
-                            Add delivery preference
-                        </button>
-                    </form>
+                    </div>
                 </div>
-            </div>
+                
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="alert"></div>
+
+                        <form id="save-delivery" data-parsley-excluded="[disabled=disabled]">
+                            <div id="delivery-setting">
+                                <div class="form-group">
+                                    <label>
+                                        Do you want to offer delivery?
+                                    </label>
+                                    
+                                    <div class="radio-box">
+                                        <label class="custom-control custom-radio">
+                                            <input name="is-offered" type="radio" value="1" class="custom-control-input" <?php if ($details['is_offered'] == 1) { echo 'checked'; } ?>>
+                                            
+                                            <span class="custom-control-indicator"></span>
+                                            <span class="custom-control-description">
+                                                Yes
+                                            </span>
+                                        </label>
+
+                                        <label class="custom-control custom-radio">
+                                            <input name="is-offered" type="radio" value="0" class="custom-control-input" <?php if ($details['is_offered'] == 0) { echo 'checked'; } ?> >
+                                            
+                                            <span class="custom-control-indicator"></span>
+                                            <span class="custom-control-description">
+                                                No
+                                            </span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div id="distance" class="setting" <?php if (!$details['is_offered']) { echo 'style="display:none;"'; } ?>>
+                                <div class="form-group">
+                                    <label for="distance">
+                                        Distance to buyer (one way)
+                                    </label>
+
+                                    <div class="input-group">
+                                        <input type="number" name="distance" class="form-control" placeholder="Enter how far you would travel" value="<?php if (!empty($details)) { echo $details['distance']; } ?>" min="1" max="10000" data-parsley-type="number" data-parsley-min="1" data-parsley-type-message="Please round this value to a whole number" data-parsley-trigger="change" <?php if (empty($details['is_offered'])) { echo 'disabled'; } ?>>
+                                        <span class="input-group-addon">miles</span>
+                                    </div>
+                                </div>
+                            </div>
+                                    
+                            <div id="delivery-type" class="setting" <?php if (!$details['is_offered']) { echo 'style="display:none;"'; } ?>>
+                                <div class="form-group">  
+                                    <label>
+                                        How do you want to offer delivery?
+                                    </label>
+
+                                    <div class="radio-box">
+                                        <label class="custom-control custom-radio">
+                                            <input name="delivery-type" type="radio" value="charge" class="custom-control-input" <?php if ($details['delivery_type'] == 'charge') { echo 'checked'; } else if (empty($details['is_offered'])) { echo 'disabled'; } ?>>
+                                            
+                                            <span class="custom-control-indicator"></span>
+                                            <span class="custom-control-description">
+                                                Charge
+                                            </span>
+                                        </label>
+
+                                        <label class="custom-control custom-radio">
+                                            <input name="delivery-type" type="radio" value="free" class="custom-control-input"  <?php if ($details['delivery_type'] == 'free') { echo 'checked'; } else if (empty($details['is_offered'])) { echo 'disabled'; } ?>>
+                                            
+                                            <span class="custom-control-indicator"></span>
+                                            <span class="custom-control-description">
+                                                Free
+                                            </span>
+                                        </label>
+                                        
+                                        <label class="custom-control custom-radio">
+                                            <input name="delivery-type" type="radio" value="conditional" class="custom-control-input" <?php if ($details['delivery_type'] == 'conditional') { echo 'checked'; } else if (empty($details['is_offered'])) { echo 'disabled'; } ?>>
+                                            
+                                            <span class="custom-control-indicator"></span>
+                                            <span class="custom-control-description">
+                                                Free under a certain distance
+                                            </span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div id="conditional-free-delivery" class="setting fee" <?php if (!$details['is_offered'] || (!empty($details['delivery_type']) && $details['delivery_type'] != 'conditional')) { echo 'style="display:none;"'; } ?>>
+                                <div class="form-group">
+                                    <label for="free-distance">
+                                        Free delivery distance (one way)
+                                    </label>
+
+                                    <div class="input-group">
+                                        <input type="number" name="free-distance" class="form-control" placeholder="Enter how far you would travel for free" value="<?php if (!empty($details)) { echo $details['free_distance']; } ?>" min="1" max="10000" data-parsley-type="number" data-parsley-min="1" data-parsley-type-message="Please round this value to a whole number" data-parsley-trigger="change" <?php if (empty($details['is_offered']) || $details['delivery_type'] != 'conditional') { echo 'disabled'; } ?>>
+                                        
+                                        <span class="input-group-addon">
+                                            miles
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div id="fee" class="setting fee" <?php if (!$details['is_offered'] || (!empty($details['delivery_type']) && $details['delivery_type'] == 'free')) { echo 'style="display:none;"'; } ?>>
+                                <div class="form-group">
+                                    <label for="fee-rate">
+                                        Set your delivery fee
+                                    </label>
+
+                                    <div class="input-group">
+                                        <div class="input-group-addon">$</div>
+                                        
+                                        <input id="fee-rate" type="text" name="fee" class="form-control" placeholder="Enter a delivery fee" value="<?php if (!empty($details)) { echo number_format($details['fee'] / 100, 2); } ?>" data-parsley-type="number" data-parlsey-min="0" data-parlsey-max="99999" data-parsley-pattern="^[0-9]+.[0-9]{2}$" data-parsley-pattern-message="Your price should include both dollars and cents (ex: $2.50)" data-parsley-trigger="change" <?php if (empty($details['is_offered']) || $details['delivery_type'] == 'free') { echo 'disabled'; } ?>>
+                                        
+                                        <select name="pricing-rate" class="input-group-addon" data-parsley-excluded="true">
+                                            <option value="per-mile" <?php if ($details['pricing_rate'] == 'per-mile') { echo 'selected'; } ?>>per mile</option>    
+                                            <option value="flat-rate" <?php if ($details['pricing_rate'] == 'flat-rate') { echo 'selected'; } ?>>flat rate</option>    
+                                        </select>
+                                    </div>
+                                    
+                                    <small id="feeHelp" class="form-text text-muted" <?php if (!$details['is_offered'] || (!empty($details['delivery_type']) && $details['delivery_type'] != 'conditional')) { echo 'style="display:none;"'; } ?>>
+                                        Delivery fee is charged only after free delivery distance threshold is reached
+                                    </small>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div> <!-- end main -->
         </div> <!-- end div.row -->
     </div> <!-- end div.container-fluid -->
