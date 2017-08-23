5d3ba7c261d65d494878a6d3775d7016ba27a3bc
EvanGrinde <evan.grinde@gmail.com>
2017-08-23 13:57:39 -0400

    tweak pickup initializer

--- routes/initializers/dashboard/exchange-settings/pickup.php
+++ routes/initializers/dashboard/exchange-settings/pickup.php
@@ -4,7 +4,7 @@ $Pickup = new Pickup([
     'DB' => $DB
 ]);
 
-if ($Pickup->exists('user_id',$User->id)){ 
+if ($Pickup->exists('user_id', $User->id)){ 
     $details = $Pickup->get_details($User->id);
 } 
 
