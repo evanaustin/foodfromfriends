e12d614c564bae1cd78211a2b036b5663203507a
EvanGrinde <evan.grinde@gmail.com>
2017-08-23 13:57:50 -0400

    refactor meetup initializer

--- routes/initializers/dashboard/exchange-settings/meetup.php
+++ routes/initializers/dashboard/exchange-settings/meetup.php
@@ -4,13 +4,9 @@ $Meetup = new Meetup([
     'DB' => $DB
 ]);
  
-$settings = $Meetup->get_settings($User->id);
 
-if ($Meetup->exists('user_id',$User->id)){ 
-    $details_1 = $Meetup->get_details($User->id, 1);
-    $details_2 = $Meetup->get_details($User->id, 2);
-    $details_3 = $Meetup->get_details($User->id, 3);
-} 
-
+if ($Meetup->exists('user_id', $User->id)) { 
+    $details = $Meetup->get_details($User->id);
+}
 
 ?>
