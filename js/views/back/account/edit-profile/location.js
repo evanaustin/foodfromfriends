8a11234e9ddd7480ca893892a998ca6afba059d2
EvanGrinde <evan.grinde@gmail.com>
2017-08-29 12:05:12 -0400

    tweak location JS

--- js/views/back/account/edit-profile/location.js
+++ js/views/back/account/edit-profile/location.js
@@ -6,7 +6,7 @@ $('#edit-location').on('submit', function(e) {
     data = $form.serialize();
 
     if ($form.parsley().isValid()) {
-        App.Ajax.post('account/edit-profile/location', data, 
+        App.Ajax.post('account/edit-profile/save-location', data, 
             function(response) {
                 App.Util.msg('Your location has been saved!', 'success');
                 App.Util.animation($('button[type="submit"]'), 'bounce');
