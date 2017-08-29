24a724f8ff1a4dc93b5ace79c8a504e55a9c2e1c
EvanGrinde <evan.grinde@gmail.com>
2017-08-29 12:04:03 -0400

    build location JS

---
+++ js/views/back/account/edit-profile/location.js
@@ -0,0 +1,21 @@
+$('#edit-location').on('submit', function(e) {
+	e.preventDefault();
+    App.Util.hideMsg();
+    
+    $form = $(this);
+    data = $form.serialize();
+
+    if ($form.parsley().isValid()) {
+        App.Ajax.post('account/edit-profile/location', data, 
+            function(response) {
+                App.Util.msg('Your location has been saved!', 'success');
+                App.Util.animation($('button[type="submit"]'), 'bounce');
+                App.Util.finishedLoading();
+            },
+            function(response) {
+                App.Util.msg(response.error, 'danger');
+                App.Util.finishedLoading();
+            }
+        );
+    }
+});	
