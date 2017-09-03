bd3b17064d1a213ed273f3420d103edd0ae2e204
EvanGrinde <evan.grinde@gmail.com>
2017-09-03 13:14:36 -0400

    tweak https reroute

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require 'config.php';
 
-if (ENV == 'prod' && (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on')) {
+if (ENV == 'prod' && (!empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'on')) {
     header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
     exit();
 }
