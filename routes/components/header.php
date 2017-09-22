d8fe65dde887a10566b2bf3fe793bab349d320d9
EvanGrinde <evan.grinde@gmail.com>
2017-09-21 22:40:04 -0400

    mobile header

--- routes/components/header.php
+++ routes/components/header.php
@@ -37,15 +37,27 @@
                     <a class="nav-link" href="#">Messages</a>
                 </li> -->
 
-                <li class="nav-item dropdown">
-                    <div class="nav-link dropdown-toggle profile" style="background-image: url('<?php echo (!empty($User->filename) ? 'https://s3.amazonaws.com/foodfromfriends/' . ENV . '/profile-photos/' . $User->filename . '.' . $User->ext . '?' . time() : PUBLIC_ROOT . 'media/placeholders/default-thumbnail.jpg'); ?>');" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></div>
-                
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item" href="<?php echo PUBLIC_ROOT . 'account/edit-profile/basic-information'; ?>">Edit profile</a>
-                        <!-- <a class="dropdown-item" href="#">Account settings</a> -->
-                        <a id="log-out" class="dropdown-item" href="#">Log out</a>
-                    </div>
-                </li>
+                <div class="hidden-lg-up">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo PUBLIC_ROOT . 'account/edit-profile/basic-information'; ?>">Account settings</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a id="log-out" class="nav-link" href="#">Log out</a>
+                    </li>
+                </div>
+
+                <div class="hidden-md-down">
+                    <li class="nav-item dropdown">
+                        <div class="nav-link dropdown-toggle profile" style="background-image: url('<?php echo (!empty($User->filename) ? 'https://s3.amazonaws.com/foodfromfriends/' . ENV . '/profile-photos/' . $User->filename . '.' . $User->ext . '?' . time() : PUBLIC_ROOT . 'media/placeholders/default-thumbnail.jpg'); ?>');" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></div>
+                    
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="<?php echo PUBLIC_ROOT . 'account/edit-profile/basic-information'; ?>">Edit profile</a>
+                            <!-- <a class="dropdown-item" href="#">Account settings</a> -->
+                            <a id="log-out" class="dropdown-item" href="#">Log out</a>
+                        </div>
+                    </li>
+                </div>
             <?php } ?>
         </ul>
     </div>
