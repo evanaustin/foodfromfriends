f2bf2c56ebb1e717e3a0c174773b0b6127234d38
EvanGrinde <evan.grinde@gmail.com>
2017-09-28 21:33:45 -0400

    build GrowerOperation class

---
+++ core/GrowerOperation.php
@@ -0,0 +1,72 @@
+<?php
+ 
+class GrowerOperation extends Base {
+    
+    protected
+        $class_dependencies,
+        $DB;
+        
+    function __construct($parameters) {
+        $this->table = 'grower_operations';
+
+        $this->class_dependencies = [
+            'DB',
+        ];
+
+        parent::__construct($parameters);
+    
+        if (isset($parameters['id'])) {
+            $this->configure_object($parameters['id']);
+            $this->populate_fully($parameters['id']);
+        }
+    }
+    
+    private function populate_fully($id) {
+        $results = $this->DB->run('
+            SELECT 
+                go.*,
+                got.title AS type,
+                goa.address_line_1,
+                goa.address_line_2,
+                goa.city,
+                goa.state,
+                goa.zipcode,
+                goa.latitude,
+                goa.longitude,
+                gopi.filename,
+                gopi.ext
+            
+            FROM grower_operations go
+            
+            LEFT JOIN grower_operation_types got
+                ON got.id = go.grower_operation_type_id
+
+            LEFT JOIN grower_operation_addresses goa
+                ON goa.grower_operation_id = go.id
+            
+            LEFT JOIN grower_operation_profile_images gopi
+                ON gopi.grower_operation_id = go.id
+            
+            WHERE go.id = :id
+            
+            LIMIT 1
+        ', [
+            'id' => $id
+        ]);
+
+        if (!isset($results[0])) return false;
+
+        foreach ($results[0] as $k => $v) $this->{$k} = $v; 
+    }
+
+    public function get_types() {
+        $results = $this->DB->run('
+            SELECT * FROM grower_operation_types
+        ');
+        
+        return (isset($results)) ? $results : false;
+    }
+
+}
+
+?>
