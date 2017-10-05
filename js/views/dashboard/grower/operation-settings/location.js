363649f32e5be00f3ea0f442bb8cfacd9fa3e63a
EvanGrinde <evan.grinde@gmail.com>
2017-10-05 13:21:25 -0400

    build operation location JS

---
+++ js/views/dashboard/grower/operation-settings/location.js
@@ -0,0 +1,23 @@
+$('#edit-location').on('submit', function(e) {
+	e.preventDefault();
+    App.Util.hideMsg();
+    
+    $form = $(this);
+    data = $form.serialize();
+
+    if ($form.parsley().isValid()) {
+        App.Util.loading();
+
+        App.Ajax.post('dashboard/grower/operation-settings/save-location', data, 
+            function(response) {
+                App.Util.msg('Your location has been saved!', 'success');
+                App.Util.animation($('button[type="submit"]'), 'bounce');
+                App.Util.finishedLoading();
+            },
+            function(response) {
+                App.Util.msg(response.error, 'danger');
+                App.Util.finishedLoading();
+            }
+        );
+    }
+});	
