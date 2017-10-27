ca6ff733daf0a726d1226b03a0441a8e0459bed7
EvanGrinde <evan.grinde@gmail.com>
2017-10-27 16:25:07 -0400

    build out map & slidebars in front JS

--- js/front.js
+++ js/front.js
@@ -1,14 +1,29 @@
-$('.js-open-right-slidebar').on('click', function(event) {
-    event.stopPropagation();
-    controller.open('slidebar-2');
-});
+App.Front = function() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZm9vZGZyb21mcmllbmRzIiwiYSI6ImNqN2twb2gwdTJmdWkzMm5wNmw0ejJ2cHEifQ.vv9p76S-5nm9ku_guP3-Pg';
 
-$('.js-close-right-slidebar').on('click', function(event) {
-    event.stopPropagation();
-    controller.close('slidebar-2');
-});
+    this.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v10',
+        zoom: 13
+    });
 
-$('.js-toggle-right-slidebar').on('click', function(event) {
-    event.stopPropagation();
-    controller.toggle('slidebar-2');
-});
+    function listener() {
+        // Create & initialize a new instance of Slidebars
+        var controller = new slidebars();
+        controller.init();
+
+        $('#cart-toggle').on('click', function(e) {
+            App.Util.slidebar(controller, 'toggle', 'right', e);
+        });
+
+        $('body').on('click', '.close-any-slidebar', function(e) {
+            e.stopPropagation();
+            controller.close();
+        });
+    };
+
+    return {
+        map: this.map,
+        listener: listener
+    };
+}();
