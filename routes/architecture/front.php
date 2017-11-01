cc86308606ae6e2a3d62838b2a05eba6739d4d25
EvanGrinde <evan.grinde@gmail.com>
2017-11-01 14:42:40 -0400

    rebuild front architecture

--- routes/architecture/front.php
+++ routes/architecture/front.php
@@ -1,15 +1,8 @@
 <?php
 
 $body = [
-    // 'header'    => 'routes/components/header',
     'view'      => 'routes/views/' . $Routing->path,
     // 'footer'    => 'routes/components/footer',
-    'modal'     => 'routes/modals/' . $Routing->path
 ];
-
-if (!$LOGGED_IN) {
-    $body['sign-up'] = 'routes/modals/sign-up';
-    $body['log-in'] = 'routes/modals/log-in';
-}
 
 ?>
