98fca7a7012900751c593807bab5c2074db1d171
EvanGrinde <evan.grinde@gmail.com>
2017-11-01 14:20:45 -0400

    fix add-to-cart ajax

--- ajax/order/add-to-cart.php
+++ ajax/order/add-to-cart.php
@@ -10,8 +10,10 @@ $json['success'] = true;
 $_POST = $Gump->sanitize($_POST);
 
 $Gump->validation_rules([
-	'food_listing_id' => 'required|integer',
-    'quantity' => 'required|integer'
+	'user_id'			=> 'required|integer',
+	'food_listing_id'	=> 'required|integer',
+    'quantity'			=> 'required|integer',
+    'exchange_option'	=> 'required|alpha'
 ]);
 
 $validated_data = $Gump->run($_POST);
@@ -21,26 +23,38 @@ if ($validated_data === false) {
 }
 
 $Gump->filter_rules([
-	'food_listing_id' => 'trim|sanitize_numbers',
-    'quantity' => 'trim|sanitize_numbers'
+	'user_id'			=> 'trim|sanitize_numbers',
+	'food_listing_id'	=> 'trim|sanitize_numbers',
+    'quantity'			=> 'trim|sanitize_numbers',
+    'exchange_option'	=> 'trim|sanitize_string'
 ]);
 
 $prepared_data = $Gump->run($validated_data);
 
+foreach ($prepared_data as $k => $v) ${str_replace('-', '_', $k)} = $v;
+
 // Add to cart
 // ----------------------------------------------------------------------------
 try {
-	$Order = new Order();
-	$Order = $Order->get_cart($User->id);
+	$Order = new Order([
+		'DB' => $DB
+	]);
 
-	$FoodListing = new FoodListing(['id' => $prepared_data['food_listing_id']]);
-	$GrowerOperation = new GrowerOperation(['id' => $FoodListing->grower_operation_id]);
+	$Order = $Order->get_cart($user_id);
 
-	$Order->add_to_cart($GrowerOperation, $FoodListing, $prepared_data['quantity']);
+	$FoodListing = new FoodListing([
+		'DB' => $DB,
+		'id' => $food_listing_id
+	]);
+
+	$GrowerOperation = new GrowerOperation([
+		'DB' => $DB,
+		'id' => $FoodListing->grower_operation_id
+	]);
+
+	$Order->add_to_cart($GrowerOperation, $FoodListing, $quantity);
 } catch (\Exception $e) {
 	quit($e->getMessage());
 }
-
-
 
 echo json_encode($json);
