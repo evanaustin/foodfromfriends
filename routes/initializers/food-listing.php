5dd5e294de1a687b98b8e726fa639a5f0a4fdece
EvanGrinde <evan.grinde@gmail.com>
2017-11-03 20:27:03 -0400

    initialize GrowerOperations w/ details & exchange in food-listing & grower

--- routes/initializers/food-listing.php
+++ routes/initializers/food-listing.php
@@ -13,61 +13,29 @@ if (isset($FoodListing->id)) {
     $GrowerOperation = new GrowerOperation([
         'DB' => $DB,
         'id' => $FoodListing->grower_operation_id
+    ], [
+        'details'   => true,
+        'exchange'  => true
     ]);
 
     if (isset($GrowerOperation->id)) {
-        $listing_title      = (!empty($FoodListing->other_subcategory)) ? $FoodListing->other_subcategory : $FoodListing->subcategory_title;
-        $listing_filename   = (!empty($FoodListing->filename)) ? 'https://s3.amazonaws.com/foodfromfriends/' . ENV . '/food-listings/' . $FoodListing->filename . '.' . $FoodListing->ext : PUBLIC_ROOT . 'media/placeholders/default-thumbnail.jpg';
-
         $exchange_options_available = [];
 
-        if ($GrowerOperation->Delivery && $GrowerOperation->Delivery->is_offered) array_push($exchange_options_available, 'delivery');
-        if ($GrowerOperation->Pickup && $GrowerOperation->Pickup->is_offered) array_push($exchange_options_available, 'pickup');
-        if ($GrowerOperation->Meetup && $GrowerOperation->Meetup->is_offered) array_push($exchange_options_available, 'meetup');
+        if ($GrowerOperation->Delivery && $GrowerOperation->Delivery->is_offered) $exchange_options_available []= 'delivery';
+        if ($GrowerOperation->Pickup && $GrowerOperation->Pickup->is_offered) $exchange_options_available []= 'pickup';
+        if ($GrowerOperation->Meetup && $GrowerOperation->Meetup->is_offered) $exchange_options_available []= 'meetup';
 
-        if ($GrowerOperation->type == 'none') {
-            $team_members = $GrowerOperation->get_team_members();
-
-            $ThisUser   = new User([
-                'DB' => $DB,
-                'id' => $team_members[0]['id']
-            ]);
-
-            $op_filename = (!empty($ThisUser->filename)) ? 'https://s3.amazonaws.com/foodfromfriends/' . ENV . '/profile-photos/' . $ThisUser->filename . '.' . $ThisUser->ext : PUBLIC_ROOT . 'media/placeholders/default-thumbnail.jpg';
-        
-            $latitude   = $ThisUser->latitude;
-            $longitude  = $ThisUser->longitude;
-        
-            $name       = $ThisUser->first_name;
-            $city       = $ThisUser->bio;
-        
-            $city       = $ThisUser->city;
-            $state      = $ThisUser->state;
-        
-            $joined_on  = $ThisUser->registered_on;
-        } else {
-            $op_filename   = (!empty($GrowerOperation->filename)) ? 'https://s3.amazonaws.com/foodfromfriends/' . ENV . '/grower-operation-images/' . $GrowerOperation->filename . '.' . $GrowerOperation->ext . '?' . time() : PUBLIC_ROOT . 'media/placeholders/default-thumbnail.jpg';
+        if (isset($User) 
+        && !empty($User->latitude) && !empty($User->longitude) 
+        && !empty($GrowerOperation->details['lat']) && !empty($GrowerOperation->details['lng'])) {
             
-            $latitude   = $GrowerOperation->latitude;
-            $longitude  = $GrowerOperation->longitude;
-        
-            $name       = $GrowerOperation->name;
-            $bio        = $GrowerOperation->bio;
-        
-            $city       = $GrowerOperation->city;
-            $state      = $GrowerOperation->state;
-        
-            $joined_on  = $GrowerOperation->created_on;
-        }
-        
-        if (isset($User) && !empty($User->latitude) && !empty($User->longitude) && !empty($latitude) && !empty($longitude)) {
             $length = getDistance([
                 'lat' => $User->latitude,
                 'lng' => $User->longitude
             ],
             [
-                'lat' => $latitude,
-                'lng' => $longitude
+                'lat' => $GrowerOperation->details['lat'],
+                'lng' => $GrowerOperation->details['lng']
             ]);
         
             if ($length < 0.1) {
@@ -96,7 +64,7 @@ if (isset($FoodListing->id)) {
             $grower_stars .= '<i class="fa fa-star"></i>';
         }
 
-        $settings['title'] = ucfirst($listing_title) . ' from ' . $name . ' | Food From Friends';
+        $settings['title'] = $FoodListing->title . ' from ' . $GrowerOperation->details['name'] . ' | Food From Friends';
     }
 }
 
