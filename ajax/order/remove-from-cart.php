7d52d50825902e3ca6fb14e3709885de911c183d
EvanGrinde <evan.grinde@gmail.com>
2017-11-07 17:48:15 -0500

    refactor remove-from-cart ajax

--- ajax/order/remove-from-cart.php
+++ ajax/order/remove-from-cart.php
@@ -10,7 +10,7 @@ $json['success'] = true;
 $_POST = $Gump->sanitize($_POST);
 
 $Gump->validation_rules([
-	'food_listing_id' => 'required|integer'
+	'food-listing-id' => 'required|integer'
 ]);
 
 $validated_data = $Gump->run($_POST);
@@ -20,24 +20,50 @@ if ($validated_data === false) {
 }
 
 $Gump->filter_rules([
-	'food_listing_id' => 'trim|sanitize_numbers'
+	'food-listing-id' => 'trim|sanitize_numbers'
 ]);
 
 $prepared_data = $Gump->run($validated_data);
 
+foreach ($prepared_data as $k => $v) ${str_replace('-', '_', $k)} = $v;
+
 // Add to cart
 // ----------------------------------------------------------------------------
 try {
-	$Order = new Order();
+	$Order = new Order([
+		'DB' => $DB
+	]);
+
 	$Order = $Order->get_cart($User->id);
 
-	$FoodListing = new FoodListing(['id' => $prepared_data['food_listing_id']]);
+	$FoodListing = new FoodListing([
+		'DB' => $DB,
+		'id' => $food_listing_id
+	]);
+
+	if (!isset($Order->Growers[$FoodListing->grower_operation_id]->FoodListings[$FoodListing->id])) {
+		quit('This item is not in your basket');
+	}
+
+	// store this before removing item from cart
+	$ordergrower_id = $Order->Growers[$FoodListing->grower_operation_id]->id;
 
 	$Order->remove_from_cart($FoodListing);
+
+	$json['ordergrower'] = [
+		'id'	=> $ordergrower_id,
+		'count'	=> ((isset($Order->Growers)) ? count($Order->Growers) : 0),
+		'items' => ((isset($Order->Growers[$FoodListing->grower_operation_id])) ? count($Order->Growers[$FoodListing->grower_operation_id]->FoodListings) : 0)
+	];
+
+	$json['order'] = [
+		'subtotal'	=> '$' . number_format($Order->subtotal / 100, 2),
+		'ex_fee'	=> '$' . number_format($Order->exchange_fees / 100, 2),
+		'fff_fee'	=> '$' . number_format($Order->fff_fee / 100, 2),
+		'total'		=> '$' . number_format($Order->total / 100, 2)
+	];
 } catch (\Exception $e) {
 	quit($e->getMessage());
 }
-
-
 
 echo json_encode($json);
