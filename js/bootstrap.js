0896a5530456434bef63194f2e249a1bd2192ff4
EvanGrinde <evan.grinde@gmail.com>
2017-11-30 14:52:35 -0500

    add ledger collapse JS

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,20 +1,31 @@
 App.Bootstrap = function() {
 	function listener() {
 		/*
-		* Enable tooltips
-		*/
+		 * Enable tooltips
+		 */
 		$(function () {
 			$('[data-toggle="tooltip"]').tooltip();
 		});
 		
 		/*
-		* Activate collapse
-		*/
+		 * Activate collapse
+         */
 		var $nav = $('.nav-item');
 
 		$nav.on('show.bs.collapse','.collapse', function() {
 			$nav.find('.collapse').collapse('hide');
-		});
+        });
+        
+        /*
+         * Ledger collapse
+         */
+        $('.ledger .collapse').on('shown.bs.collapse', function() {
+            $(this).parent().removeClass('closed').addClass('opened');
+        });
+        
+        $('.ledger .collapse').on('hidden.bs.collapse', function() {
+            $(this).not('.show').parent().removeClass('opened').addClass('closed');
+        });
 	}
 
 	return {
