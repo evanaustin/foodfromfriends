6884f43416d937995df74f6d9d1801da901aca25
EvanGrinde <evan.grinde@gmail.com>
2018-01-08 14:07:22 -0500

    get solo op for selling message inbox

--- routes/initializers/dashboard/messages/inbox/selling.php
+++ routes/initializers/dashboard/messages/inbox/selling.php
@@ -10,6 +10,15 @@ if (isset($_GET['grower'])) {
     if (isset($User->Operations[$grower_operation_id])) {
         $User->GrowerOperation = $User->Operations[$grower_operation_id];
     }
+} else if (isset($User->Operations)) {
+    foreach ($User->Operations as $Op) {
+        if ($Op->type !== 'none') {
+            continue;
+        } else {
+            $User->GrowerOperation = $Op;
+            break;
+        }
+    }
 }
 
 if (isset($User->GrowerOperation) && ((!isset($grower_operation_id) && $User->GrowerOperation->type == 'none') || $User->GrowerOperation->id == $grower_operation_id)) {
