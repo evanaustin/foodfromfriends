47dfd64275459dcb16f6b09bde39a9e126cc852f
EvanGrinde <evan.grinde@gmail.com>
2018-01-17 19:49:25 -0500

    rejection email notification

--- ajax/order/reject.php
+++ ajax/order/reject.php
@@ -31,7 +31,28 @@ try {
         'id' => $prepared_data['ordergrower_id']
     ]);
 
-	$OrderGrower->Status->reject();
+    $OrderGrower->Status->reject();
+    
+    $Buyer = new User([
+        'DB' => $DB,
+        'id' => $OrderGrower->user_id
+    ]);
+    
+    $Seller = new GrowerOperation([
+        'DB' => $DB,
+        'id' => $OrderGrower->grower_operation_id
+    ],[
+        'details' => true
+    ]);
+
+    $Mail = new Mail([
+        'fromName'  => 'Food From Friends',
+        'fromEmail' => 'foodfromfriendsco@gmail.com',
+        'toName'    => $Buyer->name,
+        'toEmail'   => $Buyer->email
+    ]);
+    
+    $Mail->rejected_order_notification($Buyer, $OrderGrower, $Seller);
 } catch (\Exception $e) {
 	quit($e->getMessage());
 }
