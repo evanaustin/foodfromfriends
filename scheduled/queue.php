d71f49ec7d0e748ce6bf9dc65e9edf894602b0d0
EvanGrinde <evan.grinde@gmail.com>
2018-01-20 18:04:10 -0500

    use At in scheduled queue

--- scheduled/queue.php
+++ scheduled/queue.php
@@ -4,6 +4,8 @@ $config = 'config.php';
 while (!file_exists($config)) $config = '../' . $config;
 require $config;
 
+use Treffynnon\At\Wrapper as At;
+
 echo '<pre>';
 print_r(At::lq());
 echo '</pre>';
