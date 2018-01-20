b15eb24e789b85945902b772a78ac149dad20bda
EvanGrinde <evan.grinde@gmail.com>
2018-01-20 18:02:03 -0500

    ch cron/test > scheduled/queue

--- scheduled/queue.php
+++ scheduled/queue.php
@@ -4,10 +4,8 @@ $config = 'config.php';
 while (!file_exists($config)) $config = '../' . $config;
 require $config;
 
-parse_str($argv, $params);
-
-foreach($params as $k => $v) {
-    error_log($v);
-}
+echo '<pre>';
+print_r(At::lq());
+echo '</pre>';
 
 ?>
