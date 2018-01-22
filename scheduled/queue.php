d50e17b61df3ba206a5e744fe9154d454c92a845
EvanGrinde <evan.grinde@gmail.com>
2018-01-22 15:27:39 -0500

    refactor scheduled queue display

--- scheduled/queue.php
+++ scheduled/queue.php
@@ -6,19 +6,34 @@ require $config;
 
 use Treffynnon\At\Wrapper as At;
 
-echo 'attempt capture';
+echo '<strong>Attempt capture</strong>';
+
 foreach(At::lq('a') as $Job) {
-    echo "<pre>{$Job->data['job_number']} : {$Job->data['date']}";
+    $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
+    $scheduled->setTimezone(new DateTimeZone('America/New_York'));
+    $date = $scheduled->format('F d, Y \a\t g:i A');
+    
+    echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
 }
 
-echo 'expire';
+echo '<strong>Expire</strong>';
+
 foreach(At::lq('b') as $Job) {
-    echo "<pre>{$Job->data['job_number']} : {$Job->data['date']}";
+    $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
+    $scheduled->setTimezone(new DateTimeZone('America/New_York'));
+    $date = $scheduled->format('F d, Y \a\t g:i A');
+    
+    echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
 }
 
-echo 'clear';
+echo '<strong>Clear</strong>';
+
 foreach(At::lq('c') as $Job) {
-    echo "<pre>{$Job->data['job_number']} : {$Job->data['date']}";
+    $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
+    $scheduled->setTimezone(new DateTimeZone('America/New_York'));
+    $date = $scheduled->format('F d, Y \a\t g:i A');
+    
+    echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
 }
 
 ?>
