5dac713cbdefe7409178e2480d9c35d72202616e
EvanGrinde <evan.grinde@gmail.com>
2018-01-29 17:52:20 -0500

    tweak format of job queue

--- scheduled/queue.php
+++ scheduled/queue.php
@@ -6,34 +6,58 @@ require $config;
 
 use Treffynnon\At\Wrapper as At;
 
-echo '<strong>Attempt capture</strong>';
-
-foreach(At::lq('a') as $Job) {
-    $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
-    $scheduled->setTimezone(new DateTimeZone('America/New_York'));
-    $date = $scheduled->format('F d, Y \a\t g:i A');
-    
-    echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
-}
-
-echo '<strong>Expire</strong>';
-
-foreach(At::lq('b') as $Job) {
-    $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
-    $scheduled->setTimezone(new DateTimeZone('America/New_York'));
-    $date = $scheduled->format('F d, Y \a\t g:i A');
-    
-    echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
-}
-
-echo '<strong>Clear</strong>';
-
-foreach(At::lq('c') as $Job) {
-    $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
-    $scheduled->setTimezone(new DateTimeZone('America/New_York'));
-    $date = $scheduled->format('F d, Y \a\t g:i A');
-    
-    echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
-}
-
 ?>
+
+<p>
+
+    <strong>Attempt capture</strong>
+
+    <?php
+
+    foreach(At::lq('a') as $Job) {
+        $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
+        $scheduled->setTimezone(new DateTimeZone('America/New_York'));
+        $date = $scheduled->format('F d, Y \a\t g:i A');
+        
+        echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
+    }
+
+    ?>
+
+</p>
+
+<p>
+
+    <strong>Expire</strong>';
+
+    <?php
+
+    foreach(At::lq('b') as $Job) {
+        $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
+        $scheduled->setTimezone(new DateTimeZone('America/New_York'));
+        $date = $scheduled->format('F d, Y \a\t g:i A');
+        
+        echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
+    }
+
+    ?>
+
+</p>
+
+<p>
+
+    <strong>Clear</strong>
+
+    <?php
+
+    foreach(At::lq('c') as $Job) {
+        $scheduled = new DateTime($Job->__get('date'), new DateTimeZone('UTC'));
+        $scheduled->setTimezone(new DateTimeZone('America/New_York'));
+        $date = $scheduled->format('F d, Y \a\t g:i A');
+        
+        echo "<pre>{$Job->__get('job_number')} : {$date}</pre>";
+    }
+
+    ?>
+
+</p>
