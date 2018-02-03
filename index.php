61ed4ae7bdf12f473684ff5f60fa93a069559414
EvanGrinde <evan.grinde@gmail.com>
2018-02-03 16:02:58 -0500

    don't do anything about https from index

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 
 require 'config.php';
 
-if ((ENV == 'prod' || ENV == 'stage') && (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on')) {
-    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-    exit();
-}
-
 $Routing = new Routing([
     'path'      => $_GET['path'],
     'landing'   => (ENV == 'prod' ? 'splash' : 'map')
