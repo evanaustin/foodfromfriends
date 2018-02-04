de4027ec86baf9ce4d14235059691ad9c9132ca1
EvanGrinde <evan.grinde@gmail.com>
2018-02-04 14:35:51 -0500

    only route to splash if prod && not logged in

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'config.php';
 
 $Routing = new Routing([
     'path'      => $_GET['path'],
-    'landing'   => (ENV == 'prod' ? 'splash' : 'map')
+    'landing'   => (ENV == 'prod' && !$LOGGED_IN) ? 'splash' : 'map'
 ]);
 
 include SERVER_ROOT . 'routes/template.php';
