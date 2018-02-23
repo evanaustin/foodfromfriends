693386a0401e2e46dd8fabf32c4b91274eb076cf
EvanGrinde <evan.grinde@gmail.com>
2018-02-23 18:49:51 -0500

    mobilize item v2

--- routes/views/item.php
+++ routes/views/item.php
@@ -10,8 +10,8 @@
                     ?>
 
                     <div class="row">
-                        <div class="col-12 order-3 col-lg-3 order-lg-1">
-                            <div id="sidebar-content">
+                        <div class="col-lg-3 order-lg-1 d-none d-md-block">
+                            <div class="sidebar-content">
                                 <div class="photo box">
                                     <?php img(ENV . '/food-listings/' . $FoodListing->filename, $FoodListing->ext, 'S3', 'img-fluid'); ?>
                                 </div>
@@ -26,7 +26,7 @@
                             </div>
                         </div>
 
-                        <div class="col-12 order-1 col-lg-5 order-lg-2">
+                        <div class="col-12 order-1 col-lg-5 order-lg-1">
                             <div id="main-content">
                                 <h2 class="dark-gray bold margin-btm-25em">
                                     <?php echo $FoodListing->title; ?>
@@ -59,12 +59,12 @@
                                 <?php
                                 
                                 if (!empty($FoodListing->description)) {
-                                    echo "<p class=\"muted margin-btm-2em\">{$FoodListing->description}</p>";
+                                    echo "<p class=\"description muted\">{$FoodListing->description}</p>";
                                 }
 
                                 ?>
 
-                                <div class="available-exchange-options set">
+                                <div class="available-exchange-options set d-none d-md-block">
                                     <h4 class="margin-btm-50em">
                                         <bold class="dark-gray">Exchange options</bold>
                                         <light class="light-gray">(<?php echo count($exchange_options_available); ?>)</light>
@@ -162,7 +162,7 @@
                                     
                                     ?>
 
-                                    <div class="reviews set">
+                                    <div class="reviews set d-none d-md-block">
                                         <h4 class="margin-btm-50em ">
                                             <bold class="dark-gray">Reviews</bold> 
                                             <light class="light-gray">(<?php echo count($ratings); ?>)</light>
@@ -210,7 +210,7 @@
 
                                 ?>
 
-                                <div class="about-grower set">
+                                <div class="about-grower set d-none d-md-block">
                                     <h4 class="margin-btm-50em ">
                                         <bold class="dark-gray">About the grower</bold> 
                                     </h4>
@@ -242,7 +242,7 @@
                             </div>    
                         </div>
 
-                        <div class="col-12 order-2 col-lg-4 order-lg-3">
+                        <div class="col-12 order-2 col-lg-4 order-lg-2">
                             <div id="basket-form-container" class="sticky-top">
                                 <div class="box">
                                     <div class="header">    
@@ -369,7 +369,193 @@
                                         ?>
                                     </div>
                                 </div>
-                            </div> <!-- end div.right-content -->
+                            </div>
+                        </div>
+
+                        <div class="col-12 order-3 d-md-none">
+                            <div class="sidebar-content">
+                                <div class="photo box">
+                                    <?php img(ENV . '/food-listings/' . $FoodListing->filename, $FoodListing->ext, 'S3', 'img-fluid'); ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 order-4 d-md-none">
+                            <div class="available-exchange-options set">
+                                <h4 class="margin-btm-50em">
+                                    <bold class="dark-gray">Exchange options</bold>
+                                    <light class="light-gray">(<?php echo count($exchange_options_available); ?>)</light>
+                                </h4>
+
+                                <div class="muted margin-btm-1em">
+                                    Available options for getting your food
+                                </div>
+                                
+                                <?php
+                                            
+                                if ($GrowerOperation->Delivery && $GrowerOperation->Delivery->is_offered) {
+
+                                    ?>
+
+                                    <div class="callout">
+                                        <div class="muted font-18 thick">
+                                            Delivery
+                                        </div>
+                                        
+                                        <div>
+                                            <?php echo "Will deliver within: {$GrowerOperation->Delivery->distance} miles"; ?>
+                                        </div>
+
+                                        <?php
+                                        
+                                        if ($GrowerOperation->Delivery->delivery_type == 'conditional') {
+                                            
+                                            echo "<div>Free delivery within: {$GrowerOperation->Delivery->free_distance} miles</div>";
+
+                                        }
+
+                                        ?>
+
+                                        <div>
+                                            <?php echo ($GrowerOperation->Delivery->delivery_type == 'free' ? 'Free' : 'Rate: $' . number_format($GrowerOperation->Delivery->fee / 100, 2) . ' ' . str_replace('-', ' ', $GrowerOperation->Delivery->pricing_rate)); ?>
+                                        </div>
+                                    </div>
+
+                                    <?php
+
+                                } if ($GrowerOperation->Pickup && $GrowerOperation->Pickup->is_offered) {
+                                    
+                                    ?>
+                                    
+                                    <div class="callout">
+                                        <div class="muted font-18 thick">
+                                            Pickup
+                                        </div>
+
+                                        <div>
+                                            <?php echo "{$GrowerOperation->details['city']}, {$GrowerOperation->details['state']}"; ?>
+                                        </div>
+                                        
+                                        <?php
+                                        
+                                        if (isset($distance) && $distance['length'] > 0) {
+                                            echo "<div>{$distance['length']} {$distance['units']} away</div>";
+                                        }
+
+                                        ?>
+                                    </div>
+
+                                    <?php
+                                    
+                                } if ($GrowerOperation->Meetup && $GrowerOperation->Meetup->is_offered) {
+                                    
+                                    ?>
+                                    
+                                    <div class="callout">
+                                        <div class="muted font-18 thick">
+                                            Meetup
+                                        </div>
+
+                                        <div>
+                                            <?php
+                                            
+                                            echo $GrowerOperation->Meetup->address_line_1 . (($GrowerOperation->Meetup->address_line_2) ? ', ' . $GrowerOperation->Meetup->address_line_2 : '') . '<br>';
+                                            echo "{$GrowerOperation->Meetup->city}, {$GrowerOperation->Meetup->state} {$GrowerOperation->Meetup->zipcode}";
+                                            
+                                            ?>
+                                        </div>
+                                    </div>
+
+                                    <?php
+                                    
+                                }
+                                
+                                ?>
+                            </div>
+
+                            <?php
+                                
+                            if (!empty($ratings) && count($ratings) > 0) {
+                                
+                                ?>
+
+                                <div class="reviews set">
+                                    <h4 class="margin-btm-50em ">
+                                        <bold class="dark-gray">Reviews</bold> 
+                                        <light class="light-gray">(<?php echo count($ratings); ?>)</light>
+                                    </h4>
+                                    
+                                    <div class="muted margin-btm-1em">
+                                        Item reviews from customers
+                                    </div>
+
+                                    <?php 
+                                    
+                                    foreach ($ratings as $rating) { 
+                                    
+                                        $ReviewUser = new User([
+                                            'DB' => $DB,
+                                            'id' => $rating['user_id']
+                                        ]);
+
+                                        ?>           
+                                        
+                                        <div class="user-block margin-btm-1em">                  
+                                            <div class="user-photo" style="background-image: url(<?php echo (!empty($ReviewUser->filename) ? 'https://s3.amazonaws.com/foodfromfriends/' . ENV . '/profile-photos/' . $ReviewUser->filename . '.' . $ReviewUser->ext /* . '?' . time() */: PUBLIC_ROOT . 'media/placeholders/default-thumbnail.jpg'); ?>);"></div>
+                                            
+                                            <div class="user-content">
+                                                <p class="muted margin-btm-25em">
+                                                    &quot;<?php echo $rating['review']; ?>&quot;
+                                                </p>
+
+                                                <small class="dark-gray bold flexstart">
+                                                    <?php echo "{$ReviewUser->first_name} &bull; {$ReviewUser->city}, {$ReviewUser->state}"; ?>
+                                                </small>
+                                            </div>
+                                        </div>
+                                        
+                                        <?php
+
+                                        }
+
+                                    ?>
+                                </div>
+
+                                <?php
+
+                                }
+
+                            ?>
+
+                            <div class="about-grower set">
+                                <h4 class="margin-btm-50em ">
+                                    <bold class="dark-gray">About the grower</bold> 
+                                </h4>
+
+                                <div class="muted margin-btm-1em">
+                                    Get to know the <?php echo (($GrowerOperation->type == 'none') ? 'person' : 'people'); ?> growing your food
+                                </div>
+
+                                <div class="user-block">
+                                    <div class="user-photo" style="background-image: url(<?php echo (!empty($GrowerOperation->details['path']) ? 'https://s3.amazonaws.com/foodfromfriends/' . ENV . $GrowerOperation->details['path'] . '.' . $GrowerOperation->details['ext'] : PUBLIC_ROOT . 'media/placeholders/default-thumbnail.jpg'); ?>);"></div>    
+                                
+                                    <div class="user-content">
+                                        <div class="font-18 muted thick">    
+                                            <a href="<?php echo PUBLIC_ROOT . $GrowerOperation->link; ?>">
+                                                <?php echo $GrowerOperation->name; ?>
+                                            </a>
+                                        </div>
+                                            
+                                        <div class="font-85 muted bold margin-btm-50em">
+                                            <?php echo "{$grower_stars} &nbsp;&bull;&nbsp; {$GrowerOperation->details['city']}, {$GrowerOperation->details['state']}"; ?>
+                                        </div>
+                                        
+                                        <p class="light-gray">
+                                            <?php echo $GrowerOperation->details['bio']; ?>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     
--- routes/views/seller-profile.php
+++ routes/views/seller-profile.php
@@ -8,7 +8,7 @@
 
             <div class="row">   
                 <div class="col-12 order-2 col-lg-3 order-lg-1">
-                    <div id="sidebar-content">
+                    <div class="sidebar-content">
                         <div class="photo box">
                             <?php img(ENV . $GrowerOperation->details['path'], $GrowerOperation->details['ext'], 'S3', 'img-fluid'); ?>
                         </div>
