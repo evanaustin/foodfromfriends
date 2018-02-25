a7b8b4717d8ebc7d6dd0b67c49d40f5a3ff29703
EvanGrinde <evan.grinde@gmail.com>
2018-02-25 17:32:43 -0500

    store/update billing info in place order ajax

--- ajax/order/place-order.php
+++ ajax/order/place-order.php
@@ -12,8 +12,14 @@ $json['success'] = true;
 $_POST = $Gump->sanitize($_POST);
 
 $Gump->validation_rules([
-    'stripe_token' => 'max_len,50',
-    'stripe_card_id' => 'max_len,50'
+    'card_name'         => 'required|alpha_space',
+    'address_line_1'    => 'required|alpha_numeric_space|max_len,35',
+    'address_line_2'    => 'alpha_numeric_space|max_len,25',
+    'city'              => 'required|alpha_space|max_len,35',
+    'state'             => 'required|regex,/^[A-Z]{2}$/',
+    'zipcode'           => 'required|regex,/^[0-9]{5}$/',
+    'stripe_token'      => 'max_len,50',
+    'stripe_card_id'    => 'max_len,50'
 ]);
 
 $validated_data = $Gump->run($_POST);
@@ -23,8 +29,14 @@ if ($validated_data === false) {
 }
 
 $Gump->filter_rules([
-	'stripe_token' => 'trim|sanitize_string',
-	'stripe_card_id' => 'trim|sanitize_string'
+    'card_name'         => 'trim|sanitize_string',
+	'address_line_1'    => 'trim|sanitize_string',
+	'address_line_2'    => 'trim|sanitize_string',
+	'city'              => 'trim|sanitize_string',
+	'state'             => 'trim|sanitize_string',
+	'zipcode'           => 'trim|whole_number',
+    'stripe_token'      => 'trim|sanitize_string',
+	'stripe_card_id'    => 'trim|sanitize_string'
 ]);
 
 $prepared_data = $Gump->run($validated_data);
@@ -40,6 +52,31 @@ if (!isset($stripe_token) || empty($stripe_token)) {
 	if (!isset($stripe_card_id) || empty($stripe_card_id)) {
 		quit('No payment method specified.');
 	}
+}
+
+if ($User->exists('user_id', $User->id, 'user_billing_info')) {
+    $updated = $User->update([
+        'card_name'         => $card_name,
+        'address_line_1'    => $address_line_1,
+        'address_line_2'    => (isset($address_line_2) ? $address_line_2 : ''),
+        'city'              => $city,
+        'state'             => $state,
+        'zipcode'           => $zipcode
+    ], 'user_id', $User->id, 'user_billing_info');
+    
+    if (!$updated) quit('We could not use this billing information');
+} else {
+    $added = $User->add([
+        'card_name'         => $card_name,
+        'user_id'           => $User->id,
+        'address_line_1'    => $address_line_1,
+        'address_line_2'    => $address_line_2,
+        'city'              => $city,
+        'state'             => $state,
+        'zipcode'           => $zipcode
+    ], 'user_billing_info');
+    
+    if (!$added) quit('We could not add your billing information');
 }
 
 // Perform Stripe charge, mark order as paid, initialize payout data
@@ -120,7 +157,7 @@ try {
                 'toEmail'   => $Member->email
             ]);
             
-            $Mail->new_order_notification($Member, $Seller, $OrderGrower, $User);
+            // $Mail->new_order_notification($Member, $Seller, $OrderGrower, $User);
         }
     }
 } catch (\Exception $e) {
