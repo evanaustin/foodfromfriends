39240719f21d608e7ba5b972980eb95a1bdf93de
EvanGrinde <evan.grinde@gmail.com>
2018-03-09 15:25:05 -0500

    refactor Routing profile checks into separate functions

--- core/Routing.php
+++ core/Routing.php
@@ -15,8 +15,6 @@ class Routing extends Base {
         $page;
 
     public
-        $buyer_profiles,
-        $seller_profiles,
         $profile_type,
         $buyer_type,
         $seller_type,
@@ -29,8 +27,6 @@ class Routing extends Base {
         $item_variety,
         $item_type,
         $item_id;
-
-    public $assns;
 
     function __construct($parameters) {
         $this->DB = $parameters['DB'];
@@ -59,115 +55,124 @@ class Routing extends Base {
             $this->template     = 'front';
             $this->section      = $exp_path[0];
 
-            // Manually set buyer types (just individuals for now)
-            $this->buyer_profiles = [
-                'user'
-            ];
+            $this->is_buyer_profile($exp_path);
+            $this->is_seller_profile($exp_path);
+        }
+    }
 
-            // Check if section is a buyer profile
-            if (in_array($this->section, $this->buyer_profiles)) {
-                $this->profile_type = 'buyer';
-                $this->buyer_type   = $this->section;
+    private function is_buyer_profile($exp_path) {
+        // manually set buyer types (just individuals for now)
+        $buyer_profiles = [
+            'user'
+        ];
 
-                if (isset($exp_path[1])) {
-                    $this->buyer    = $exp_path[1];
-                    $this->path     = 'buyer-profile';
-                }
+        // check profile
+        if (in_array($this->section, $buyer_profiles)) {
+            $this->profile_type = 'buyer';
+            $this->buyer_type   = $this->section;
+
+            if (isset($exp_path[1])) {
+                $this->buyer    = $exp_path[1];
+                $this->path     = 'buyer-profile';
             }
+        } else {
+            return false;
+        }
+    }
 
-            // Retrieve & index seller operation types + catch solo ops
-            $seller_types = $this->retrieve([
-                'table' => 'grower_operation_types'
-            ]);
+    private function is_seller_profile($exp_path) {
+        // retrieve & index seller operation types + catch solo ops
+        $seller_types = $this->retrieve([
+            'table' => 'grower_operation_types'
+        ]);
 
-            foreach($seller_types as $type) {
-                $this->seller_profiles[$type['id']] = $type['title'];
-            }
+        foreach($seller_types as $type) {
+            $seller_profiles[$type['id']] = $type['title'];
+        }
 
-            $this->seller_profiles []= 'grower';
+        $seller_profiles []= 'grower';
 
-            // Check if section is a seller profile
-            if (in_array($this->section, $this->seller_profiles)) {
-                $this->profile_type = 'seller';
-                $this->seller_type  = $this->section;
+        // check profile
+        if (in_array($this->section, $seller_profiles)) {
+            $this->profile_type = 'seller';
+            $this->seller_type  = $this->section;
 
-                // Check if seller slug is set
-                if (isset($exp_path[1])) {
-                    $this->seller = $exp_path[1];
+            // Check if seller slug is set
+            if (isset($exp_path[1])) {
+                $this->seller = $exp_path[1];
+                
+                // Check if item category slug is set
+                if (isset($exp_path[2])) {
+                    $Item = new FoodListing([
+                        'DB' => $this->DB
+                    ]);
                     
-                    // Check if item category slug is set
-                    if (isset($exp_path[2])) {
-                        $Item = new FoodListing([
-                            'DB' => $this->DB
-                        ]);
-                        
-                        $Slug = new Slug([
-                            'DB' => $this->DB
-                        ]);
-                                
-                        // Get item category:subcateory associations & construct data structure to map relationships
-                        $raw_assns      = $Item->get_category_associations();
-                        $category_assns = [];
-                        // $this->assns = $raw_assns;
-                        foreach ($raw_assns as $assn) {
-                            $category_slug      = $Slug->slugify($assn['category_title']);
-                            $subcategory_slug   = $Slug->slugify($assn['subcategory_title']);
+                    $Slug = new Slug([
+                        'DB' => $this->DB
+                    ]);
                             
-                            if (isset($assn['variety_id'])) {
-                                $variety_slug   = $Slug->slugify($assn['variety_title']);
-                                $category_assns[$category_slug][$subcategory_slug][$variety_slug] = $assn['variety_id'];
-                            } else {
-                                $category_assns[$category_slug][$subcategory_slug] = $assn['subcategory_id'];
-                            }
+                    // Get item category:subcateory associations & construct data structure to map relationships
+                    $raw_assns      = $Item->get_category_associations();
+                    $category_assns = [];
+                    
+                    foreach ($raw_assns as $assn) {
+                        $category_slug      = $Slug->slugify($assn['category_title']);
+                        $subcategory_slug   = $Slug->slugify($assn['subcategory_title']);
+                        
+                        if (isset($assn['variety_id'])) {
+                            $variety_slug   = $Slug->slugify($assn['variety_title']);
+                            $category_assns[$category_slug][$subcategory_slug][$variety_slug] = $assn['variety_id'];
+                        } else {
+                            $category_assns[$category_slug][$subcategory_slug] = $assn['subcategory_id'];
                         }
+                    }
 
-                        $this->assns = $category_assns;
+                    // Check if item subcategory slug is set
+                    if (isset($category_assns[$exp_path[2]])) {
+                        $this->item_category = $exp_path[2];
                         
-                        // Check if item subcategory slug is set
-                        if (isset($category_assns[$exp_path[2]])) {
-                            $this->item_category = $exp_path[2];
-                            
-                            // Check if given subcategory actually belongs to given category
-                            if (isset($exp_path[3], $category_assns[$exp_path[2]][$exp_path[3]])) {
-                                // Item page
-                                $this->path                 = 'item';
-                                $this->item_subcategory     = $exp_path[3];
+                        // Check if given subcategory actually belongs to given category
+                        if (isset($exp_path[3], $category_assns[$exp_path[2]][$exp_path[3]])) {
+                            // Item page
+                            $this->path                 = 'item';
+                            $this->item_subcategory     = $exp_path[3];
 
-                                if (isset($exp_path[4])) {
-                                    if (is_array($category_assns[$exp_path[2]][$exp_path[3]]) && isset($category_assns[$exp_path[2]][$exp_path[3]][$exp_path[4]])) {
-                                        // Variety page
-                                        $this->item_variety = $exp_path[4];
-                                        $this->item_type    = 'variety';
-                                        $this->item_id      = $category_assns[$exp_path[2]][$exp_path[3]][$exp_path[4]];
-                                    } else {
-                                        // Subcategory:variety mis-association
-                                        // (direct to seller profile for now)
-                                        $this->path = 'seller-profile';
-                                        error_log('Subcategory:variety mis-association');
-                                    }
+                            if (isset($exp_path[4])) {
+                                if (is_array($category_assns[$exp_path[2]][$exp_path[3]]) && isset($category_assns[$exp_path[2]][$exp_path[3]][$exp_path[4]])) {
+                                    // Variety page
+                                    $this->item_variety = $exp_path[4];
+                                    $this->item_type    = 'variety';
+                                    $this->item_id      = $category_assns[$exp_path[2]][$exp_path[3]][$exp_path[4]];
                                 } else {
-                                    // Subcategory page
-                                    $this->item_type        = 'subcategory';
-                                    $this->item_id          = $category_assns[$exp_path[2]][$exp_path[3]];
+                                    // Subcategory:variety mis-association
+                                    // (direct to seller profile for now)
+                                    $this->path = 'seller-profile';
+                                    error_log('Subcategory:variety mis-association');
                                 }
                             } else {
-                                // Category:subcategory mis-association
-                                // (direct to seller profile for now)
-                                $this->path = 'seller-profile';
+                                // Subcategory page
+                                $this->item_type        = 'subcategory';
+                                $this->item_id          = $category_assns[$exp_path[2]][$exp_path[3]];
                             }
                         } else {
-                            // Seller category page
+                            // Category:subcategory mis-association
+                            // (direct to seller profile for now)
                             $this->path = 'seller-profile';
                         }
                     } else {
-                        // Seller profile
+                        // Seller category page
                         $this->path = 'seller-profile';
                     }
                 } else {
-                    // Seller type directory
-                    // (seller type is set without a seller)
+                    // Seller profile
+                    $this->path = 'seller-profile';
                 }
+            } else {
+                // Seller type directory
+                // (seller type is set without a seller)
             }
+        } else {
+            return false;
         }
     }
 }
