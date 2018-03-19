d458fc942ebaf3ee5077478a3c540e7d50203edb
EvanGrinde <evan.grinde@gmail.com>
2018-03-19 01:11:01 -0400

    don't retrieve payout settings if user op isn't set on grower dashboard

--- routes/initializers/dashboard/grower.php
+++ routes/initializers/dashboard/grower.php
@@ -4,31 +4,33 @@ $settings = [
     'title' => 'Seller dashboard | Food From Friends'
 ];
 
-$listing_count      = ($User->GrowerOperation) ? $User->GrowerOperation->count_listings() : 0;
-$team_member_count  = ($User->GrowerOperation) ? count($User->GrowerOperation->get_team_members()) : 0;
+$listing_count      = (isset($User->GrowerOperation)) ? $User->GrowerOperation->count_listings() : 0;
+$team_member_count  = (isset($User->GrowerOperation)) ? count($User->GrowerOperation->get_team_members()) : 0;
 
-$payout_settings = $User->GrowerOperation->retrieve([
-    'where' => [
-        'seller_id' => $User->GrowerOperation->id
-    ],
-    'table' => 'seller_payout_settings',
-    'limit' => 1
-]);
+if (isset($User->GrowerOperation)) {
+    $payout_settings = $User->GrowerOperation->retrieve([
+        'where' => [
+            'seller_id' => $User->GrowerOperation->id
+        ],
+        'table' => 'seller_payout_settings',
+        'limit' => 1
+    ]);
+}
 
 $requirements = [
-    'add a profile photo' =>  [
+    'add a profile photo' => [
         'link'      => 'dashboard/grower/settings/edit-profile',
         'status'    => (!empty($User->GrowerOperation->filename) ? 'complete' : 'incomplete'),
     ],
-    'set your location' =>  [
+    'set your location' => [
         'link'      => 'dashboard/grower/settings/edit-profile',
         'status'    => (!empty($User->GrowerOperation->zipcode) ? 'complete' : 'incomplete'),
     ],
-    'upload your first listing' =>  [
+    'upload your first listing' => [
         'link'      => 'dashboard/grower/items/add-new',
         'status'    => (($listing_count > 0) ? 'complete' : 'incomplete'),
     ],
-    'enable at least one exchange option' =>  [
+    'enable at least one exchange option' => [
         'link'      => 'dashboard/grower/exchange-options/pickup',
         'status'    => (!empty($User->GrowerOperation->Delivery->is_offered) || !empty($User->GrowerOperation->Pickup->is_offered) || !empty($User->GrowerOperation->Meetup->is_offered) ? 'complete' : 'incomplete'),
     ],
