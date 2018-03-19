29d2e815703fbe60b8fb1daf57cab255e66ed3f1
EvanGrinde <evan.grinde@gmail.com>
2018-03-18 21:52:36 -0400

    show bank account number on payout settings page

--- routes/views/dashboard/grower/settings/payout-settings.php
+++ routes/views/dashboard/grower/settings/payout-settings.php
@@ -60,7 +60,7 @@
                                         Bank Account
                                     </label>
 
-                                    <input type="text" name="bank-account" class="form-control" placeholder="Bank account number" value="<?php if (!empty($payout_settings['bank_account_number'])) echo $payout_settings['bank_account_number']; ?>" data-parsley-trigger="submit" required>
+                                    <input type="text" name="bank-account" class="form-control" placeholder="Bank account number" value="<?php if (!empty($payout_settings['account_number'])) echo $payout_settings['account_number']; ?>" data-parsley-trigger="submit" required>
                                 </div>
                             </div>
                         </div>
