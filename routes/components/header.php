fc7d50a70d885d9b7c6093d79c69ebdb8f1afa48
EvanGrinde <evan.grinde@gmail.com>
2018-03-18 21:23:30 -0400

    link to map from header & nav

--- routes/components/header.php
+++ routes/components/header.php
@@ -12,7 +12,7 @@
 
             <?php if (!$LOGGED_IN) { ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo PUBLIC_ROOT; ?>">
+                    <a class="nav-link" href="<?php echo PUBLIC_ROOT . 'map'; ?>">
                         <span>
                             Map
                         </span>
@@ -45,7 +45,7 @@
                 <li class="nav-item d-none d-md-block">
                     <a 
                         class="nav-link <?php if ($Routing->section == 'map') { echo 'active'; } ?>" 
-                        href="<?php echo PUBLIC_ROOT; ?>"
+                        href="<?php echo PUBLIC_ROOT . 'map'; ?>"
                         data-toggle="tooltip" data-placement="bottom" title="Map"
                     >
                         <i class="fa fa-map"></i>
