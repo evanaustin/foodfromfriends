fabc9e0634a12e3b6f9105493c9e68f6e88ab946
EvanGrinde <evan.grinde@gmail.com>
2018-03-20 17:17:16 -0400

    body imgs & copy v2

--- routes/views/home.php
+++ routes/views/home.php
@@ -3,7 +3,7 @@
         <div class="hero container-fluid">
             <div class="row">
                 <div class="d-none col-md-3 d-md-flex flexcenter no-padding">
-                    <?php img('homepage/veggies', 'png', 'local', 'img-fluid veggies'); ?>
+                    <?php img('homepage/veggies', 'png', 'local', 'veggies'); ?>
                 </div>
 
                 <div class="col-8 col-md-6 d-flex flexjustifycenter flexcolumn no-padding">
@@ -25,7 +25,7 @@
                 </div>
 
                 <div class="col-4 col-md-3 d-md-flex flexcenter flexend no-padding">
-                    <?php img('homepage/colorbowl-half', 'png', 'local', 'img-fluid colorbowl'); ?>
+                    <?php img('homepage/colorbowl-half', 'png', 'local', 'colorbowl'); ?>
                 </div>
             </div>
 
@@ -42,14 +42,12 @@
         </div>
 
         <div class="sidekick container-fluid">
-            <div class="row">
-                <!-- <div class="col-12 col-md-2 no-padding">
-                    <?php //img('homepage/chard', 'png', 'local', 'chard text-left'); ?>
-                </div> -->
-
-                <div class="col-12 col-md-7 no-padding shift-up">
+            <div id="thought" class="row">
+                <div class="col-12 col-md-1 no-padding">
                     <?php img('homepage/chard', 'png', 'local', 'chard text-left'); ?>
+                </div>
 
+                <div class="col-12 col-md-7 no-padding">
                     <div id="thought-for-food">
                         <h1>
                             Thought<br>
@@ -69,35 +67,60 @@
                             It shouldn't be.
                         </p>
                     </div>
-
-                    <?php img('homepage/artichoke-plate', 'png', 'local', 'plate'); ?>
                 </div>
 
-                <div class="col-12 col-md-5">
+                <div class="col-12 col-md-4 no-padding">
                     <?php img('homepage/tomatoes', 'png', 'local', 'img-fluid tomatoes text-right'); ?>
+                </div>
+            </div>
 
-                    <div id="the-big-problems">
-                        <h1>
-                            Tackling the<br>
-                            <strong class="uppercase">big problems</strong>
-                        </h1>
-                        
-                        <p>
-                            Food is our fuel &mdash; the source of our energy as living beings. Yet the energy we spend in food transportation is <strong>10 times</strong> greater than the energy we receive in eating. The average American meal is sourced from <strong>1,500 miles</strong> away. By the time food ends up on our plates it's lost not only flavor but also nutritional quality.
-                        </p>
+            <div id="problems" class="row">
+                <div class="col-12 col-md-4 no-padding">
+                    <?php img('homepage/square-plate', 'png', 'local', 'plate'); ?>
+                </div>
+    
+                <div class="col-12 col-md-4">
+                    <div id="the-big-problems" class="text-right">
+                        <div class="row">
+                            <div class="col-12 no-padding">
+                                <h1>
+                                    Tackling the<br>
+                                    <strong class="uppercase">big problems</strong>
+                                </h1>
+                                
+                                <p>
+                                    Food is our fuel &mdash; the source of our energy as living beings. And yet the energy we spend transporting food is <strong>10 times</strong> greater than the energy we receive in eating it. The average American meal is sourced from <strong>1,500 miles</strong> away.
+                                </p>
+                            </div>
+                        </div>
 
-                        <p>
-                            This is a resource-intensive, unhealthy, non-transparent food system. It doesn't need to be this way.
-                        </p>
+                        <div class="row">
+                            <div class="col-11 offset-1 no-padding">
+                                <p>
+                                    By the time this agro-industrial food ends up on our plates, it's lost much of the limited flavor and nutrition that it may have had to begin with.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-10 offset-2 no-padding">
+                                <p>
+                                    Food by nature is intimate and rich in story, but our modern food system is commoditized, resource-intensive, and not healthy.
+                                </p>
+                                    
+                                <p>
+                                    It doesn't need to be this way.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        
-        <div id="model-cake" class="sidekick container-fluid">
-            <div class="row">
-                <!-- <div class="col-12 col-md-1">
-                </div> -->
+
+            <div id="model" class="row">
+                <div class="col-12 col-md-5 no-padding text-center">
+                    <?php img('homepage/smoothie', 'png', 'local', 'img-fluid smoothie'); ?>
+                </div>
 
                 <div class="col-12 col-md-6">
                     <div id="model-for-the-future">
@@ -105,21 +128,26 @@
                             A history-proven model<br>
                             <strong class="uppercase">for the future</strong>
                         </h1>
-
+    
                         <p>
                             As recently as <strong>1945</strong>, nearly half of the fresh produce in America was grown in home and community gardens. Our production was so abundant that during World War II we shipped food overseas to Europe to aid our allies in their food shortages.
                         </p>
+    
+                        <p>
+                            In about <strong>30 years</strong> food demand is expected to increase by up to <strong>98%</strong>. Some suggest clear-cutting even more forested land for greater commercial productivity. We believe the solution has been right under our noses (and in our backyards) the whole time.
+                        </p>
 
                         <p>
-                            Furthermore, in about <strong>30 years</strong> food demand is expected to increase by up to <strong>98%</strong>. Some suggest clear-cutting even more land for greater commercial productivity. We believe the solution (and now the technology) is already here.
+                            Now the technology to make it a reality is here, too.
                         </p>
                     </div>
                 </div>
-
-                <div class="col-12 col-md-6 text-center">
-                    <?php img('homepage/coffee-cake', 'png', 'local', 'img-fluid'); ?>
-                </div>
+    
+                <!-- <div class="col-12 col-md-6 no-padding">
+                    <?php //img('homepage/loaf', 'png', 'local', 'img-fluid loaf'); ?>
+                </div> -->
             </div>
         </div>
+
     </div>
 </main>
