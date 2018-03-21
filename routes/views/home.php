8b7b978648da6cecdd1d6ddaf8bbc246016b1fc4
EvanGrinde <evan.grinde@gmail.com>
2018-03-20 22:18:28 -0400

    main img & copy v3 + mobilize

--- routes/views/home.php
+++ routes/views/home.php
@@ -1,57 +1,47 @@
 <main>
-    <div class="main">
-        <div class="hero container-fluid">
+    <div class="main container-fluid">
+        <div id="hero">
             <div class="row">
                 <div class="d-none col-md-3 d-md-flex flexcenter no-padding">
-                    <?php img('homepage/veggies', 'png', 'local', 'veggies'); ?>
+                    <?php img('homepage/veggies', 'png', 'local', 'img-fluid veggies'); ?>
                 </div>
 
-                <div class="col-8 col-md-6 d-flex flexjustifycenter flexcolumn no-padding">
+                <div class="col-8 col-md-6 d-flex flexjustifycenter flexcolumn md-no-padding">
                     <h1>
-                        the new way<br>
+                        the new way<br class="d-none d-md-inline">
                         to buy & sell<br>
-                        <strong class="uppercase">local food</strong>
+                        <strong>local food</strong>
                     </h1>
 
-                    <div class="row d-none d-md-flex">
-                        <div class="col-md-6 col-xl-4">
+                    <div class="row d-md-flex">
+                        <div class="col-12 col-md-6 col-xl-4">
                             <a href="<?php echo PUBLIC_ROOT . 'map'; ?>" class="btn btn-primary btn-block btn-lg">See what's for sale</a>
                         </div>
 
-                        <div class="col-md-6 col-xl-3">
+                        <div class="col-12 col-md-6 col-xl-3">
                             <a href="<?php echo PUBLIC_ROOT . 'dashboard/grower/items/add-new'; ?>" class="btn btn-dark btn-block btn-lg start-selling">Start selling</a>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-4 col-md-3 d-md-flex flexcenter flexend no-padding">
-                    <?php img('homepage/colorbowl-half', 'png', 'local', 'colorbowl'); ?>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-3 d-block d-md-none no-padding">
-                    <?php img('homepage/veggies', 'png', 'local', 'img-fluid veggies'); ?>
-                </div>
-
-                <div class="col-9 d-flex flexjustifycenter flexcolumn d-md-none">
-                    <a href="<?php echo PUBLIC_ROOT . 'map'; ?>" class="btn btn-primary btn-block">See what's for sale</a>
-                    <a href="<?php echo PUBLIC_ROOT . 'dashboard/grower/items/add-new'; ?>" class="btn btn-dark btn-block start-selling">Start selling</a>
+                <div class="col-4 col-md-3 d-md-flex flexcenter flexend no-padding text-right">
+                    <?php img('homepage/colorbowl-phone', 'png', 'local', 'colorbowl img-fluid d-block d-md-none'); ?>
+                    <?php img('homepage/colorbowl', 'png', 'local', 'colorbowl d-none d-md-block'); ?>
                 </div>
             </div>
         </div>
 
-        <div class="sidekick container-fluid">
+        <div id="sidekick">
             <div id="thought" class="row">
                 <div class="col-12 col-md-1 no-padding">
-                    <?php img('homepage/chard', 'png', 'local', 'chard text-left'); ?>
+                    <?php img('homepage/chard', 'png', 'local', 'chard'); ?>
                 </div>
 
-                <div class="col-12 col-md-7 no-padding">
+                <div class="col-12 col-md-7 md-no-padding">
                     <div id="thought-for-food">
                         <h1>
                             Thought<br>
-                            <strong class="uppercase">for food</strong>
+                            <strong>for food</strong>
                         </h1>
                         
                         <p>
@@ -60,7 +50,6 @@
 
                         <p>
                             Are we afraid to ask these questions because of the answers we may get? Or is there simply no one to ask? Buying food these days is a mysterious, detached experience.
-                            <!-- The average American meal consists of food shipped in from <strong>1,500 miles</strong> away. By the time it eventually ends up on our plates it's lost not only flavor but also nutritional quality. -->
                         </p>
 
                         <p>
@@ -69,33 +58,34 @@
                     </div>
                 </div>
 
-                <div class="col-12 col-md-4 no-padding">
-                    <?php img('homepage/tomatoes', 'png', 'local', 'img-fluid tomatoes text-right'); ?>
+                <div class="col-md-4 d-none d-md-flex flexend no-padding">
+                    <?php img('homepage/tomatoes', 'png', 'local', 'img-fluid tomatoes'); ?>
                 </div>
             </div>
 
             <div id="problems" class="row">
                 <div class="col-12 col-md-4 no-padding">
-                    <?php img('homepage/square-plate', 'png', 'local', 'plate'); ?>
+                    <?php img('homepage/square-plate-phone', 'png', 'local', 'plate img-fluid d-block d-md-none'); ?>
+                    <?php img('homepage/square-plate', 'png', 'local', 'plate d-none d-md-block'); ?>
                 </div>
     
                 <div class="col-12 col-md-4">
-                    <div id="the-big-problems" class="text-right">
+                    <div id="the-big-problems">
                         <div class="row">
-                            <div class="col-12 no-padding">
+                            <div class="col-12 col-md-10 offset-md-2 md-no-padding">
                                 <h1>
                                     Tackling the<br>
-                                    <strong class="uppercase">big problems</strong>
+                                    <strong>big problems</strong>
                                 </h1>
                                 
                                 <p>
-                                    Food is our fuel &mdash; the source of our energy as living beings. And yet the energy we spend transporting food is <strong>10 times</strong> greater than the energy we receive in eating it. The average American meal is sourced from <strong>1,500 miles</strong> away.
+                                    Food is our fuel &mdash; the source of our energy as living beings. Yet the energy we spend transporting food is <strong>10 times</strong> greater than the amount of energy we receive in eating it. The average American meal is sourced from <strong>1,500 miles</strong> away.
                                 </p>
                             </div>
                         </div>
 
                         <div class="row">
-                            <div class="col-11 offset-1 no-padding">
+                            <div class="col-12 col-md-9 offset-md-3 md-no-padding">
                                 <p>
                                     By the time this agro-industrial food ends up on our plates, it's lost much of the limited flavor and nutrition that it may have had to begin with.
                                 </p>
@@ -103,7 +93,7 @@
                         </div>
 
                         <div class="row">
-                            <div class="col-10 offset-2 no-padding">
+                            <div class="col-12 col-md-9 offset-md-3 md-no-padding">
                                 <p>
                                     Food by nature is intimate and rich in story, but our modern food system is commoditized, resource-intensive, and not healthy.
                                 </p>
@@ -115,18 +105,22 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="col-12 d-flex d-md-none flexend no-padding">
+                    <?php img('homepage/chard-bunch', 'png', 'local', 'chard-bunch'); ?>
+                </div>
             </div>
 
             <div id="model" class="row">
-                <div class="col-12 col-md-5 no-padding text-center">
-                    <?php img('homepage/smoothie', 'png', 'local', 'img-fluid smoothie'); ?>
+                <div class="col-md-4 d-none d-md-block">
+                    <?php img('homepage/smoothie', 'png', 'local', 'smoothie'); ?>
                 </div>
 
                 <div class="col-12 col-md-6">
                     <div id="model-for-the-future">
                         <h1>
-                            A history-proven model<br>
-                            <strong class="uppercase">for the future</strong>
+                            A history-proven model <span class="d-inline d-md-none">for</span><br>
+                            <strong><span class="d-none d-md-inline">for</span> the future</strong>
                         </h1>
     
                         <p>
@@ -142,12 +136,41 @@
                         </p>
                     </div>
                 </div>
-    
-                <!-- <div class="col-12 col-md-6 no-padding">
-                    <?php //img('homepage/loaf', 'png', 'local', 'img-fluid loaf'); ?>
-                </div> -->
+            </div>
+                    
+            <div id="action" class="row">
+                <div class="col-12 col-md-5 order-2 order-md-1">
+                    <div id="practice">
+                        <h2>
+                            A practice as <strong class="dark-gray">old</strong> as humanity itself,<br class="d-none d-md-inline">
+                            revitalized as a <strong class="dark-gray">new</strong> side-hustle
+                        </h2>
+
+                        <div class="row d-block d-md-flex flexend">
+                            <div class="col-12 col-md-6 col-xl-5">
+                                <a href="<?php echo PUBLIC_ROOT . 'map'; ?>" class="btn btn-primary btn-block btn-lg">See what's for sale</a>
+                            </div>
+
+                            <div class="col-12 col-md-6 col-xl-4">
+                                <a href="<?php echo PUBLIC_ROOT . 'dashboard/grower/items/add-new'; ?>" class="btn btn-dark btn-block btn-lg start-selling">Start selling</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-7 order-1 order-md-2 col-md-flex flexend no-padding text-right">
+                    <?php img('homepage/loaf-end', 'png', 'local', 'loaf img-fluid d-block d-md-none'); ?>
+                    <?php img('homepage/loaf', 'png', 'local', 'loaf d-none d-md-inline'); ?>
+                </div>
             </div>
         </div>
-
     </div>
 </main>
+
+<nav id="footer" class="navbar">
+    <ul class="navbar-nav ml-auto mr-auto">
+        <li class="nav-item">
+            © Food From Friends, Inc.
+        </li>
+    </ul>
+</nav>
