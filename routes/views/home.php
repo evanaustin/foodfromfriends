f9f30baf4578d65052c4ea4740aa1fcdfe6c5e4c
EvanGrinde <evan.grinde@gmail.com>
2018-03-21 03:16:27 -0400

    full responsiveness

--- routes/views/home.php
+++ routes/views/home.php
@@ -2,42 +2,43 @@
     <div class="main container-fluid">
         <div id="hero">
             <div class="row">
-                <div class="d-none col-md-3 d-md-flex flexcenter no-padding">
+                <div class="d-none col-lg-3 d-lg-flex flexcenter no-padding">
                     <?php img('homepage/veggies', 'jpg', 'local', 'img-fluid veggies'); ?>
                 </div>
 
-                <div class="col-8 col-md-6 d-flex flexjustifycenter flexcolumn md-no-padding">
+                <div class="col-8 col-lg-6 d-flex flexjustifycenter flexcolumn lg-no-padding">
                     <h1>
-                        the new way<br class="d-none d-md-inline">
+                        the new way<br class="d-none d-sm-inline">
                         to buy & sell<br>
                         <strong>local food</strong>
                     </h1>
 
                     <div class="row d-md-flex">
-                        <div class="col-12 col-md-6 col-xl-4">
+                        <div class="col-12 col-lg-6 col-xl-4">
                             <a href="<?php echo PUBLIC_ROOT . 'map'; ?>" class="btn btn-primary btn-block btn-lg">See what's for sale</a>
                         </div>
 
-                        <div class="col-12 col-md-6 col-xl-3">
+                        <div class="col-12 col-lg-6 col-xl-3">
                             <a href="<?php echo PUBLIC_ROOT . 'dashboard/grower/items/add-new'; ?>" class="btn btn-dark btn-block btn-lg start-selling">Start selling</a>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-4 col-md-3 d-md-flex flexcenter flexend no-padding text-right">
-                    <?php img('homepage/colorbowl-phone', 'jpg', 'local', 'colorbowl img-fluid d-block d-md-none'); ?>
-                    <?php img('homepage/colorbowl', 'png', 'local', 'colorbowl d-none d-md-block'); ?>
+                <div class="col-4 col-lg-3 d-lg-flex flexcenter flexend no-padding text-right">
+                    <?php img('homepage/colorbowl-phone', 'jpg', 'local', 'colorbowl img-fluid d-inline d-md-none'); ?>
+                    <?php img('homepage/colorbowl', 'png', 'local', 'colorbowl img-fluid d-none d-md-inline d-xl-none'); ?>
+                    <?php img('homepage/colorbowl', 'png', 'local', 'colorbowl d-none d-xl-inline'); ?>
                 </div>
             </div>
         </div>
 
         <div id="sidekick">
             <div id="thought" class="row">
-                <div class="col-12 col-md-1 no-padding">
+                <div class="col-12 col-sm-3 d-md-none col-lg-1 d-lg-inline no-padding">
                     <?php img('homepage/chard', 'jpg', 'local', 'chard'); ?>
                 </div>
 
-                <div class="col-12 col-md-7 md-no-padding">
+                <div class="col-12 col-sm-9 col-md-6 offset-md-2 col-lg-7 offset-lg-0 lg-no-padding">
                     <div id="thought-for-food">
                         <h1>
                             Thought<br>
@@ -64,15 +65,15 @@
             </div>
 
             <div id="problems" class="row">
-                <div class="col-12 col-md-4 no-padding">
-                    <?php img('homepage/square-plate-phone', 'jpg', 'local', 'plate img-fluid d-block d-md-none'); ?>
-                    <?php img('homepage/square-plate', 'jpg', 'local', 'plate d-none d-md-block'); ?>
+                <div class="col-12 col-sm-6 col-lg-3 col-xl-4 no-padding">
+                    <?php img('homepage/square-plate-phone', 'jpg', 'local', 'plate img-fluid d-inline d-sm-none'); ?>
+                    <?php img('homepage/square-plate', 'jpg', 'local', 'plate d-none d-sm-inline'); ?>
                 </div>
     
-                <div class="col-12 col-md-4">
+                <div class="col-12 col-sm-6 col-lg-5 col-xl-4">
                     <div id="the-big-problems">
                         <div class="row">
-                            <div class="col-12 col-md-10 offset-md-2 md-no-padding">
+                            <div class="col-12 col-lg-10 offset-lg-2 lg-no-padding">
                                 <h1>
                                     Tackling the<br>
                                     <strong>big problems</strong>
@@ -85,7 +86,7 @@
                         </div>
 
                         <div class="row">
-                            <div class="col-12 col-md-9 offset-md-3 md-no-padding">
+                            <div class="col-12 col-lg-9 offset-lg-3 lg-no-padding">
                                 <p>
                                     By the time this agro-industrial food ends up on our plates, it's lost much of the limited flavor and nutrition that it may have had to begin with.
                                 </p>
@@ -93,7 +94,7 @@
                         </div>
 
                         <div class="row">
-                            <div class="col-12 col-md-9 offset-md-3 md-no-padding">
+                            <div class="col-12 col-lg-9 offset-lg-3 lg-no-padding">
                                 <p>
                                     Food by nature is intimate and rich in story, but our modern food system is commoditized, resource-intensive, and not healthy.
                                 </p>
@@ -106,21 +107,29 @@
                     </div>
                 </div>
 
-                <div class="col-12 d-flex d-md-none flexend no-padding">
-                    <?php img('homepage/chard-bunch', 'jpg', 'local', 'chard-bunch'); ?>
-                </div>
+                <!-- <div class="col-12 d-md-none">
+                    <?php //img('homepage/loaf', 'jpg', 'local', 'loaf img-fluid d-none d-sm-inline'); ?>
+                </div> -->
+
+                <!-- <div class="col-6 d-lg-none">
+                    <?php //img('homepage/smoothie', 'jpg', 'local', 'smoothie img-fluid'); ?>
+                </div> -->
+
+                <!-- <div class="d-none col-lg-4 d-lg-flex d-xl-none flexbottom flexend no-padding">
+                    <?php //img('homepage/chard-bunch', 'jpg', 'local', 'chard-bunch'); ?>
+                </div> -->
             </div>
 
             <div id="model" class="row">
-                <div class="col-md-4 d-none d-md-block">
+                <div class="col-12 d-inline d-sm-none col-lg-4 d-lg-inline">
                     <?php img('homepage/smoothie', 'jpg', 'local', 'smoothie'); ?>
                 </div>
 
-                <div class="col-12 col-md-6">
+                <div class="col-12 col-sm-7 col-md-8 col-lg-7 col-lg-6">
                     <div id="model-for-the-future">
                         <h1>
-                            A history-proven model <span class="d-inline d-md-none">for</span><br>
-                            <strong><span class="d-none d-md-inline">for</span> the future</strong>
+                            A history-proven model <span class="d-inline d-sm-none">for</span><br>
+                            <strong><span class="d-none d-sm-inline">for</span> the future</strong>
                         </h1>
     
                         <p>
@@ -136,31 +145,39 @@
                         </p>
                     </div>
                 </div>
+
+                <div class="col-12 col-sm-5 col-md-4 d-lg-none d-flex flexend no-padding">
+                    <?php img('homepage/chard-bunch', 'jpg', 'local', 'chard-bunch img-fluid'); ?>
+                </div>
             </div>
                     
             <div id="action" class="row">
-                <div class="col-12 col-md-5 order-2 order-md-1">
+                <div class="d-none col-md-6 d-md-inline d-lg-none">
+                    <?php img('homepage/smoothie', 'jpg', 'local', 'smoothie img-fluid'); ?>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-6 col-xl-5">
                     <div id="practice">
                         <h2>
-                            A practice as <strong class="dark-gray">old</strong> as humanity itself,<br class="d-none d-md-inline">
+                            A practice as <strong class="dark-gray">old</strong> as humanity itself,<br class="d-none d-sm-inline d-md-none d-xl-inline">
                             revitalized as a <strong class="dark-gray">new</strong> side-hustle
                         </h2>
 
-                        <div class="row d-block d-md-flex flexend">
-                            <div class="col-12 col-md-6 col-xl-5">
+                        <div class="row d-sm-flex flexend">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6 col-xl-5">
                                 <a href="<?php echo PUBLIC_ROOT . 'map'; ?>" class="btn btn-primary btn-block btn-lg">See what's for sale</a>
                             </div>
 
-                            <div class="col-12 col-md-6 col-xl-4">
+                            <div class="col-12 col-sm-6 col-md-12 col-lg-6 col-xl-4">
                                 <a href="<?php echo PUBLIC_ROOT . 'dashboard/grower/items/add-new'; ?>" class="btn btn-dark btn-block btn-lg start-selling">Start selling</a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-12 col-md-7 order-1 order-md-2 col-md-flex flexend no-padding text-right">
-                    <?php img('homepage/loaf-phone', 'jpg', 'local', 'loaf img-fluid d-block d-md-none'); ?>
-                    <?php img('homepage/loaf', 'jpg', 'local', 'loaf d-none d-md-inline'); ?>
+                <div class="d-none col-lg-6 d-lg-flex col-xl-7 flexend no-padding text-right">
+                    <?php img('homepage/loaf-phone', 'jpg', 'local', 'loaf img-fluid d-inline d-sm-none'); ?>
+                    <?php img('homepage/loaf', 'jpg', 'local', 'loaf img-fluid d-none d-sm-inline'); ?>
                 </div>
             </div>
         </div>
