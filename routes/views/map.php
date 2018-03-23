eba5954db1f780d26dab7c45ab9ce2c5a6a10343
EvanGrinde <evan.grinde@gmail.com>
2018-03-23 17:23:36 -0400

    hide build wishlist btn on map

--- routes/views/map.php
+++ routes/views/map.php
@@ -57,13 +57,13 @@
 
                             ?>
 
-                            <div class="<?php echo (!isset($User->GrowerOperation) ? 'col-' . (empty($wishlist) ? '6' : '12') : 'hidden'); ?>">
-                                <a id="start-selling" href="<?php echo PUBLIC_ROOT . 'dashboard/grower/items/add-new'; ?>" class="btn btn-cta btn-block margin-top-btm-50em">Start selling</a>
+                            <div class="<?php echo (!isset($User->GrowerOperation) ? 'col-12' : 'hidden'); ?>">
+                                <a id="start-selling" href="<?php echo PUBLIC_ROOT . 'dashboard/grower/items/add-new'; ?>" class="btn btn-cta btn-block margin-top-btm-50em">List your items</a>
                             </div>
 
-                            <div class="<?php echo (empty($wishlist) ? 'col-' . (!isset($User->GrowerOperation) ? '6' : '12') : 'hidden'); ?>">
-                                <a id="build-wish-list" href="<?php echo PUBLIC_ROOT . 'dashboard/account/buying/wish-list'; ?>" class="btn btn-cta btn-block margin-top-btm-50em">Build a wish list</a>
-                            </div>
+                            <!-- <div class="<?php //echo (empty($wishlist) ? 'col-12' : 'hidden'); ?>">
+                                <a id="build-wish-list" href="<?php //echo PUBLIC_ROOT . 'dashboard/account/buying/wish-list'; ?>" class="btn btn-cta btn-block margin-top-btm-50em">Build a wish list</a>
+                            </div> -->
                         </div>
                     </div>
 
