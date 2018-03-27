7cc18437cbc671a381afeaba09f8b4ff94a6714c
EvanGrinde <evan.grinde@gmail.com>
2018-03-27 19:21:38 -0400

    specify bootbox close button

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,6 @@
 App.Dashboard = function() {
     function listener() {
-        $(document).on('shown.bs.modal', function (event) {
+        $(document).on('shown.bs.modal', '.bootbox.modal', function() {
             $(this).find('.modal-header').append("<button type='button' class='bootbox-close-button close' data-dismiss='modal'>&times;</button>");
         });
     };
