26cd8b690f1c697972588da2440bf9dabca88293
EvanGrinde <evan.grinde@gmail.com>
2018-03-27 15:30:41 -0400

    only load payout settings if operation is set

--- routes/initializers/dashboard/grower/settings/payout-settings.php
+++ routes/initializers/dashboard/grower/settings/payout-settings.php
@@ -4,12 +4,14 @@ $settings = [
     'title' => 'Payout settings | Food From Friends'
 ];
 
-$payout_settings = $User->GrowerOperation->retrieve([
-    'where' => [
-        'seller_id' => $User->GrowerOperation->id
-    ],
-    'table' => 'seller_payout_settings',
-    'limit' => 1
-]);
+if ($User->GrowerOperation) {
+    $payout_settings = $User->GrowerOperation->retrieve([
+        'where' => [
+            'seller_id' => $User->GrowerOperation->id
+        ],
+        'table' => 'seller_payout_settings',
+        'limit' => 1
+    ]);
+}
 
 ?>
