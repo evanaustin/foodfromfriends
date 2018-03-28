230dae40dab58dcfe3bf34715fb696e1eda3a5bb
EvanGrinde <evan.grinde@gmail.com>
2018-03-28 19:46:31 -0400

    handle meta descriptions in template

--- routes/template.php
+++ routes/template.php
@@ -52,8 +52,17 @@ foreach ([
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <meta name="google-site-verification" content="UmsixiFqd2YXf2qI8LFn_5Q4R-iDnuwQiPwdlDxTCvI" />
-        <title><?php if (isset($settings['title'])) echo $settings['title']; ?></title>
+        <meta name="google-site-verification" content="UmsixiFqd2YXf2qI8LFn_5Q4R-iDnuwQiPwdlDxTCvI"/>
+        
+        <?php
+        
+        if (isset($settings['meta-description'])) {
+            echo "<meta name=\"description\" content=\"{$settings['meta-description']}\"/>";
+        }
+
+        ?>
+        
+        <title><?php echo (!empty($settings['title']) ? $settings['title'] : 'Food From Friends'); ?></title>
         <link rel="shortcut icon" href="<?php echo PUBLIC_ROOT; ?>media/logos/favicon-32.png" type="image/x-icon">
 
         <?php if (ENV == 'prod') { ?>
