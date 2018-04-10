6d8eab01dcfe6f33c170849e2cf191ae6af93334
EvanGrinde <evan.grinde@gmail.com>
2018-04-10 15:35:10 -0400

    tweak home meta description

--- routes/initializers/home.php
+++ routes/initializers/home.php
@@ -2,7 +2,7 @@
 
 $settings = [
     'title' => 'Food From Friends',
-    'meta-description' => 'Food From Friends makes it easy and convenient for anyone to buy and sell local food online. Explore local food offerings near your or list an item for sale.'
+    'meta-description' => 'Food From Friends makes it easy and convenient for anyone to buy and sell local food online. Explore local food offerings near you or list an item for sale.'
 ];
 
 ?>
