61658f33b3750b45f33851f5d642f9441d35d6f8
EvanGrinde <evan.grinde@gmail.com>
2018-04-17 15:23:41 -0400

    build wholesale/team-members initializer, view & JS

---
+++ routes/initializers/dashboard/buying/wholesale/team-members.php
@@ -0,0 +1,11 @@
+<?php
+
+$settings = [
+    'title' => 'Your team members | Food From Friends'
+];
+
+if ($User->GrowerOperation) {
+    $team_members = $User->GrowerOperation->get_team_members();
+}
+
+?>
