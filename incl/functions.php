6a3808f3b3c639a3541aef1dd1505ff4bef18451
EvanGrinde <evan.grinde@gmail.com>
2018-04-19 12:21:19 -0400

    start shifting to modified amount() & img()

--- incl/functions.php
+++ incl/functions.php
@@ -23,6 +23,10 @@ function amount($amount, $dollar = true) {
     echo ($dollar ? '$' : ' ') . number_format($amount / 100, 2);
 }
 
+function _amount($amount, $dollar = true) {
+    return ($dollar ? '$' : ' ') . number_format($amount / 100, 2);
+}
+
 /*
  * $data = [
  *  server
@@ -35,7 +39,7 @@ function img($path, $ext, $params) {
 }
 
 function _img($path, $ext, $params) {
-    echo '<img src="' . (($params['server'] == 'local') ? PUBLIC_ROOT . 'media/' : 'https://s3.amazonaws.com/foodfromfriends/') . $path . '.' . $ext . '"' . (!empty($params['class']) ? 'class="' . $params['class'] . '"' : '') . (!empty($params['title']) ? 'title="' . $params['title'] . '"' : '') .'/>';
+    return '<img src="' . (($params['server'] == 'local') ? PUBLIC_ROOT . 'media/' : 'https://s3.amazonaws.com/foodfromfriends/') . $path . '.' . $ext . '"' . (!empty($params['class']) ? 'class="' . $params['class'] . '"' : '') . (!empty($params['title']) ? 'title="' . $params['title'] . '"' : '') .'/>';
 }
 
 function svg($path) {
