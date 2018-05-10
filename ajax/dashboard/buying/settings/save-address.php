0ec7f5ae91928730e4df3a3991d2b4fd59ee10da
EvanGrinde <evan.grinde@gmail.com>
2018-05-10 18:36:03 -0400

    calculate distance if specified in buying save address ajax

--- ajax/dashboard/buying/settings/save-address.php
+++ ajax/dashboard/buying/settings/save-address.php
@@ -17,6 +17,7 @@ $Gump->validation_rules([
     'city'              => 'required|alpha_space|max_len,35',
     'state'             => 'required|regex,/^[a-zA-Z]{2}$/',
     'zipcode'           => 'required|regex,/^[0-9]{5}$/',
+    'seller-account-id' => 'integer'
 ]);
 
 $validated_data = $Gump->run($_POST);
@@ -30,7 +31,8 @@ $Gump->filter_rules([
 	'address-line-2'    => 'trim|sanitize_string',
 	'city'              => 'trim|sanitize_string',
 	'state'             => 'trim|sanitize_string',
-	'zipcode'           => 'trim|whole_number',
+    'zipcode'           => 'trim|whole_number',
+    'seller-account-id' => 'trim|sanitize_numbers'
 ]);
 
 $prepared_data = $Gump->run($validated_data);
@@ -39,52 +41,30 @@ foreach ($prepared_data as $k => $v) ${str_replace('-', '_', $k)} = $v;
 
 
 /*
- * Update BuyerAccount:Address
+ * Add BuyerAccount:Address
  */
 
-if (!$User->BuyerAccount->Address
-    || ($address_line_1 != $User->BuyerAccount->Address->address_line_1)
-    || ($address_line_2 != $User->BuyerAccount->Address->address_line_2)
-    || ($city           != $User->BuyerAccount->Address->city)
-    || ($state          != $User->BuyerAccount->Address->state)
-    || ($zipcode        != $User->BuyerAccount->Address->zipcode)
-) {
-    $full_address = "{$address_line_1}, {$city}, {$state}";
-    $prepared_address = str_replace(' ', '+', $full_address);
+$full_address = "{$address_line_1}, {$city}, {$state}";
+$prepared_address = str_replace(' ', '+', $full_address);
+
+$geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address=' . $prepared_address . '&key=' . GOOGLE_MAPS_KEY);
+$output= json_decode($geocode);
+
+$lat = $output->results[0]->geometry->location->lat;
+$lng = $output->results[0]->geometry->location->lng;
     
-    $geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address=' . $prepared_address . '&key=' . GOOGLE_MAPS_KEY);
-    $output= json_decode($geocode);
-    
-    $lat = $output->results[0]->geometry->location->lat;
-    $lng = $output->results[0]->geometry->location->lng;
-    
-    if ($User->BuyerAccount->Address) {
-        $updated = $User->BuyerAccount->update([
-            'address_line_1'    => $address_line_1,
-            'address_line_2'    => (isset($address_line_2) ? $address_line_2 : ''),
-            'city'              => ucwords(strtolower($city)),
-            'state'             => strtoupper($state),
-            'zipcode'           => $zipcode,
-            'latitude'          => $lat,
-            'longitude'         => $lng
-        ], 'buyer_account_id', $User->BuyerAccount->id, 'buyer_account_addresses');
-        
-        if (!$updated) quit('We could not update your location');
-    } else {
-        $added = $User->BuyerAccount->add([
-            'buyer_account_id'  => $User->BuyerAccount->id,
-            'address_line_1'    => $address_line_1,
-            'address_line_2'    => $address_line_2,
-            'city'              => $city,
-            'state'             => $state,
-            'zipcode'           => $zipcode,
-            'latitude'          => $lat,
-            'longitude'         => $lng
-        ], 'buyer_account_addresses');
-        
-        if (!$added) quit('We could not add your location');
-    }
-}
+$added = $User->BuyerAccount->add([
+    'buyer_account_id'  => $User->BuyerAccount->id,
+    'address_line_1'    => $address_line_1,
+    'address_line_2'    => $address_line_2,
+    'city'              => $city,
+    'state'             => $state,
+    'zipcode'           => $zipcode,
+    'latitude'          => $lat,
+    'longitude'         => $lng
+], 'buyer_account_addresses');
+
+if (!$added) quit('We could not add your location');
 
 
 /*
@@ -96,6 +76,28 @@ $User = new User([
     'id' => $User->id,
     'buyer_account' => true
 ]);
+
+/*
+ * Calculate distance
+ */
+
+if (!empty($seller_account_id)) {
+    $SellerAccount = new GrowerOperation([
+        'DB' => $DB,
+        'id' => $seller_account_id
+    ], [
+        'exchange'  => true
+    ]);
+    
+    if (isset($User, $User->BuyerAccount, $User->BuyerAccount->Address, $User->BuyerAccount->Address->latitude, $User->BuyerAccount->Address->longitude)) {
+        $geocode    = file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={$User->BuyerAccount->Address->latitude},{$User->BuyerAccount->Address->longitude}&destinations={$SellerAccount->latitude},{$SellerAccount->longitude}&key=" . GOOGLE_MAPS_KEY);
+        $output     = json_decode($geocode);
+        $distance   = explode(' ', $output->rows[0]->elements[0]->distance->text);
+        $distance_miles = round((($distance[1] == 'ft') ? $distance[0] / 5280 : $distance[0]), 4);
+    }
+
+    $json['distance_miles'] = $distance_miles;
+}
 
 $json['slug'] = $slug;
 
