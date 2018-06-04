e1c319b8eaec930d2faf41205b46731f9ff7a818
EvanGrinde <evan.grinde@gmail.com>
2018-06-04 12:46:37 -0400

    refactor add new item js

--- js/views/dashboard/selling/items/add-new.js
+++ js/views/dashboard/selling/items/add-new.js
@@ -1,214 +1,203 @@
-// Initialize imaging
-App.Image.init();
+App.Dashboard.AddItem = function() {
+    function listener() {
+        var img_added = false;
+
+        // Initialize imaging
+        App.Image.init();
+
+        $(window).resize(function(e) {
+            App.Image.init();
+
+            if (img_added) {
+                $('.image-box input[type=file]').trigger('change');
+            }
+        });
+
+        // Upload image
+        $('.image-box').on('click', function(e) {
+            // only one image so key is always 0
+            var key = 0;
+            
+            if (App.Image.uploadDisabled[0] === false) {
+                App.Image.selectFile($(this));
+            }
+        });
+
+        $('.image-box input[type=file]').on('click', function(e) {
+            e.stopImmediatePropagation();
+        });
+
+        $('.image-box input[type=file]').on('change', function(e) {
+            App.Image.onceSelected($(this), e);
+            img_added = true;
+        });
 
 
-// Upload image
-$('div.image-box').on('click', function(e) {
-    // only one image so key is always 0
-    var key = 0;
-    
-    if (App.Image.uploadDisabled[0] === false) {
-        App.Image.selectFile($(this));
-    }
-});
-
-$('div.image-box input[type=file]').on('click', function(e) {
-    e.stopImmediatePropagation();
-});
-
-$('div.image-box input[type=file]').on('change', function(e) {
-    var success = App.Image.onceSelected($(this), e);
-});
+        // Discard uploaded image
+        $('a.remove-image').on('click', function(e) {
+            e.preventDefault();
+            App.Image.discard();
+        });
 
 
-// Discard uploaded image
-$('a.remove-image').on('click', function(e) {
-    e.preventDefault();
-    App.Image.discard();
-});
+        // Re-populate subcategory select menu
+        $('#categories').on('change', function() {
+            $('#subcategories').prop('disabled', false).empty().focus().append('<option selected disabled>Select subcategory</option>');
+
+            subcategories.forEach(function(sub) {
+                if ($(this).val() == sub.item_category_id) {
+                    $('#subcategories').append($('<option>', {
+                        value: sub.id, 
+                        text: sub.title.charAt(0).toUpperCase() + sub.title.slice(1)
+                    }));
+                } 
+            }, this);
+        });
 
 
-// Re-populate subcategory select menu
-$('#item-categories').on('change', function() {
-    $('#item-subcategories').prop('disabled', false).empty().focus().append('<option selected disabled>Select subcategory</option>');
+        // Re-populate varieties select menu
+        $('#subcategories').on('change', function() {
+            $('#name').attr('placeholder', subcategories[$('#subcategories').val() - 1]['title']).css('textTransform', 'capitalize');;
 
-    item_subcategories.forEach(function(sub) {
-         if ($(this).val() == sub.food_category_id) {
-            $('#item-subcategories').append($('<option>', {
-                value: sub.id, 
-                text: sub.title.charAt(0).toUpperCase() + sub.title.slice(1)
-            }));
-        } 
-    }, this);
-});
+            $('#varieties').empty()
+                .append('<option selected disabled>Select variety</option>')
+                .append('<option value="0">None</option>');
 
+                var varieties_exist = false;
 
-// Re-populate varieties select menu
-$('#item-subcategories').on('change', function() {
-    $('#item-varieties').empty()
-        .append('<option selected disabled>Select variety</option>')
-        .append('<option value="0">None</option>');
+                varieties.forEach(function(vari) {
+                    if ($(this).val() == vari.item_subcategory_id) {
+                        varieties_exist = true;
 
-    var varieties = false;
+                        $('#varieties').append($('<option>', {
+                            value:  vari.id, 
+                            text:   vari.title.charAt(0).toUpperCase() + vari.title.slice(1)
+                        }));
+                    } 
+                }, this);
 
-    item_varieties.forEach(function(vari) {
-        if ($(this).val() == vari.food_subcategory_id) {
-            varieties = true;
+                if (varieties_exist == true) {
+                $('#varieties').prop('disabled', false).focus().removeClass('hidden');
+            } else {
+                $('#varieties').prop('disabled', true).empty().append('<option selected disabled>(no varieties)</option>').addClass('hidden');
+            }
+        });
 
-            $('#item-varieties').append($('<option>', {
-                value:  vari.id, 
-                text:   vari.title.charAt(0).toUpperCase() + vari.title.slice(1)
-            }));
-        } 
-    }, this);
-
-    if (varieties == true) {
-        // are varieties
-        $('#item-varieties').prop('disabled', false).focus().removeClass('hidden');
-    } else {
-        // are varieties
-        $('#item-varieties').prop('disabled', true).empty().append('<option selected disabled>(no varieties)</option>').addClass('hidden');
-    }
-});
+        $('#varieties').on('change', function() {
+            if ($(this).val() > 0) {
+                $('#name').attr('placeholder', varieties[$(this).val() - 1]['title'] + ' ' + subcategories[$('#subcategories').val() - 1]['title']).css('textTransform', 'capitalize');;
+            } else {
+                $('#name').attr('placeholder', subcategories[$('#subcategories').val() - 1]['title']).css('textTransform', 'capitalize');;
+            }
+        });
 
 
-// Display other input field
-$('#food-subcategories').on('change', function() {
-    if ($(this).val() == 0) {
-        if ($('#other-option').is(':not(:visible)')) {
-            App.Util.animation($('#other-option'), 'flipInX', 'in');
-            $('#other-subcategory').focus();
-        }
-    } else {
-        if ($('#other-option').is(':visible')) {
-            App.Util.animation($('#other-option'), 'fadeOut', 'out');
-        }
-    }
-});
+        // Add item
+        $('#add-item').on('submit', function(e) {
+            e.preventDefault();
+            App.Util.hideMsg();
 
+            $form = $(this);
 
-// Check if other already exists as a subcategory
-/* $('#other-subcategory').on('keyup change', function() {
-    var other = $(this);
-
-    $('#food-subcategories option').each(function(id, el) {
-        if (el.text.charAt(0).toUpperCase() + el.text.slice(1) == other.val().charAt(0).toUpperCase() + other.val().slice(1)) {
-            el.setAttribute('selected', true);
-            other.val('');
-            App.Util.animation($('#other-option'), 'fadeOut', 'out');
-            toastr.info(el.text + ' already exists as an option');
-
-            return false;
-        } else {
-            el.removeAttribute('selected');
-        }
-    });
-}); */
-
-
-// Update availability as quantity is changed
-$('#quantity').on('keyup change', function() {
-    if ($(this).val() == 0) {
-        $('#available').prop('checked', false);
-        $('#unavailable').prop('checked', true);
-    } else {
-        $('#available').prop('checked', true);
-        $('#unavailable').prop('checked', false);
-    } 
-});
-
-
-// Add listing
-$('#add-listing').on('submit', function(e) {
-    e.preventDefault();
-    App.Util.hideMsg();
-
-    $form = $(this);
-
-    if (window.FormData){
-        formdata = new FormData($form[0]);
-        
-        if (App.Image.files.length > 0) {
-            $.each(App.Image.files, function(k, v) {
-                formdata.append('img' + k, v);
-            });
-    
-            formdata.append('images', JSON.stringify(App.Image.getCropData()));
-        }
-
-        data = formdata;
-    } else {
-        data = $form.serialize();
-    }
-
-    if ($form.parsley().isValid()) {
-        App.Util.loading();
-        
-        App.Ajax.postFiles('dashboard/selling/items/add-new', data, 
-            function(response) {
-                if (response.is_active) {
-                    App.Util.msg('Your listing has been created! Click <a href="' + PUBLIC_ROOT + response.link + '">here</a> to view it now', 'success');
-                } else {
-                    App.Util.msg('Your listing has been created! Click <a href="' + PUBLIC_ROOT + response.link + '">here</a> to view it now or <a href="' + PUBLIC_ROOT + 'dashboard/selling/">here</a> to finish activating your seller profile', 'success');
+            if (window.FormData){
+                formdata = new FormData($form[0]);
+                
+                if (App.Image.files.length > 0) {
+                    $.each(App.Image.files, function(k, v) {
+                        formdata.append('img' + k, v);
+                    });
+            
+                    formdata.append('images', JSON.stringify(App.Image.getCropData()));
                 }
 
-                App.Util.animation($('button[type="submit"]'), 'bounce');
-                App.Util.finishedLoading();
+                if ($('.suggested-photo').hasClass('active')) {
+                    if ($('.suggested-photo.active').length > 1) {
+                        console.log
+                        App.Util.msg('Please select only one photo', 'danger');
+                        return;
+                    }
 
-                // clear form
-                $form.each(function() {
-                    this.reset();
-                    $('div.form-group').removeClass('has-success');
-                    $('input, select').removeClass('success');
-                });
+                    formdata.append('similar-photo', $('.suggested-photo.active').data('image-id'));
+                }
 
-                // reset subcategories
-                $('#food-subcategories').prop('disabled', true).empty().focus().append('<option selected disabled>Select a food subcategory</option>');
-
-                // hide other option
-                $('#other-option').fadeOut();
-
-                // clear image
-                App.Image.discard();
-            },
-            function(response) {
-                App.Util.msg(response.error, 'danger');
-                App.Util.finishedLoading();
+                data = formdata;
+            } else {
+                data = $form.serialize();
             }
-        );
-    }
-});
 
+            if ($form.parsley().isValid()) {
+                App.Util.loading();
+                
+                App.Ajax.postFiles('dashboard/selling/items/add-new', data, 
+                    function(response) {
+                        App.Util.msg('Your item has been created! Click <a href="' + PUBLIC_ROOT + response.link + '">here</a> to view it now', 'success');
 
-$('#suggest-item-modal').on('shown.bs.modal', function () {
-    $('#suggest-item-form').siblings('.alerts').find('.alert').remove();
-});
+                        App.Util.animation($('button[type="submit"]'), 'bounce');
+                        App.Util.finishedLoading();
 
+                        if (response.new_image) {
+                            $('#similar-items').find('.row').append(
+                                '<div class="col-md-4">' +
+                                    '<div class="image-box suggested-photo margin-top-2em" data-image-id="' + response.new_image.id + '" data-toggle="tooltip" data-title="Use this photo" data-placement="bottom">' +
+                                        '<img src="https://s3.amazonaws.com/foodfromfriends/' + ENV + '/item-images/' + response.new_image.filename + '.' + response.new_image.ext + '" class="img-fluid rounded"/>' +
+                                    '</div>' +
+                                '</div>'
+                            );
+                        }
 
-// Suggest new item type
-$('#suggest-item-form').on('submit', function(e) {
-    e.preventDefault();
-    App.Util.hideMsg();
+                        // clear form
+                        /* $form.each(function() {
+                            this.reset();
+                            $('div.form-group').removeClass('has-success');
+                            $('input, select').removeClass('success');
+                        }); */
 
-    $form = $(this);
-
-    var data = $form.serialize();
-
-    if ($form.parsley().isValid()) {
-        App.Util.loading('.suggest-item-submit');
-        
-        App.Ajax.post('dashboard/selling/items/suggest-item', data, 
-            function(response) {
-                $('input[name="item-type"]').val('');
-                $('textarea[name="comments"]').val('');
-
-                App.Util.msg('Thanks for the suggestion! We\'ll take a look and let you know if we decide to add it', 'success', $('#suggest-item-form'));
-                App.Util.finishedLoading('.suggest-item-submit');
-            },
-            function(response) {
-                App.Util.msg(response.error, 'danger', $('#suggest-item-form'));
-                App.Util.finishedLoading('.suggest-item-submit');
+                        App.Image.discard();
+                    },
+                    function(response) {
+                        App.Util.msg(response.error, 'danger');
+                        App.Util.finishedLoading();
+                    }
+                );
             }
-        );
+        });
+
+
+        $('#suggest-item-modal').on('shown.bs.modal', function () {
+            $('#suggest-item-form').siblings('.alerts').find('.alert').remove();
+        });
+
+
+        // Suggest new item type
+        $('#suggest-item-form').on('submit', function(e) {
+            e.preventDefault();
+            App.Util.hideMsg();
+
+            $form = $(this);
+
+            var data = $form.serialize();
+
+            if ($form.parsley().isValid()) {
+                App.Util.loading('.suggest-item-submit');
+                
+                App.Ajax.post('dashboard/selling/items/suggest-item', data, 
+                    function(response) {
+                        $('input[name="type"]').val('');
+                        $('textarea[name="comments"]').val('');
+
+                        App.Util.msg('Thanks for the suggestion! We\'ll take a look and let you know if we decide to add it', 'success', $('#suggest-item-form'));
+                        App.Util.finishedLoading('.suggest-item-submit');
+                    },
+                    function(response) {
+                        App.Util.msg(response.error, 'danger', $('#suggest-item-form'));
+                        App.Util.finishedLoading('.suggest-item-submit');
+                    }
+                );
+            }
+        });
     }
-});
+
+    return {
+        listener: listener
+    };
+}();
