f4134827a90085d44d47dd4117b5de72980bd463
EvanGrinde <evan.grinde@gmail.com>
2018-06-04 12:40:06 -0400

    refactor add/edit item ajax

--- ajax/dashboard/selling/items/bulk-edit.php
+++ ajax/dashboard/selling/items/bulk-edit.php
@@ -13,10 +13,16 @@ foreach($_POST['items'] as $id => $item) {
     $item = $Gump->sanitize($item);
     
     $Gump->validation_rules([
-        'retail-price'      => 'required|regex,/^[0-9]+.[0-9]{2}$/|min_numeric, 0|max_numeric, 1000000',
-        'retail-quantity'   => 'required|regex,/^[0-9]+$/|min_numeric, 0|max_numeric, 10000',
-        'wholesale-price'   => 'regex,/^[0-9]+.[0-9]{2}$/|min_numeric, 0|max_numeric, 1000000',
-        'wholesale-quantity' => 'required|regex,/^[0-9]+$/|min_numeric, 0|max_numeric, 10000'
+        'position'      => 'integer',
+        'variety-id'    => 'integer',
+        'name'          => 'regex,/^[a-zA-z\s:]+$/',
+        'measurement'   => 'regex,/^([0-9]*[.x\s])*[0-9]+$/|max_len, 10',
+        'metric'        => 'integer',
+        'package-type'  => 'required|integer',
+        'price'         => 'required|regex,/^[0-9]+.[0-9]{2}$/|min_numeric, 0|max_numeric, 1000000',
+        'quantity'      => 'regex,/^[0-9]+$/|min_numeric, 0|max_numeric, 10000',
+        'is_available'  => 'alpha',
+        'is_wholesale'  => 'alpha'
     ]);
     
     $validated_data = $Gump->run($item);
@@ -26,15 +32,22 @@ foreach($_POST['items'] as $id => $item) {
     }
     
     $Gump->filter_rules([
-        'retail-price'      => 'trim|sanitize_floats',
-        'retail-quantity'   => 'trim|whole_number',
-        'wholesale-price'   => 'trim|sanitize_floats',
-        'wholesale-quantity' => 'trim|whole_number'
+        'position'      => 'trim|whole_number',
+        'variety-id'    => 'trim|whole_number',
+        'name'          => 'trim|sanitize_string',
+        'package-type'  => 'trim|whole_number',
+        'measurement'   => 'trim|sanitize_string',
+        'metric'        => 'trim|whole_number',
+        'price'         => 'trim|sanitize_floats',
+        'quantity'      => 'trim|whole_number',
+        'is_available'  => 'trim|sanitize_string',
+        'is_wholesale'  => 'trim|sanitize_string'
     ]);
     
     $prepared_data = $Gump->run($validated_data);
     
-    unset($available);
+    // reset non-required fields
+    unset($position, $variety_id, $name, $measurement, $metric, $quantity, $is_available, $is_wholesale);
 
     foreach ($prepared_data as $k => $v) ${str_replace('-', '_', $k)} = $v;
     
@@ -44,11 +57,16 @@ foreach($_POST['items'] as $id => $item) {
     ]);
     
     $updated = $Item->update([
-        'price'             => $retail_price * 100,
-        'quantity'          => $retail_quantity,
-        'wholesale_price'   => $wholesale_price * 100,
-        'wholesale_quantity' => $wholesale_quantity,
-        'is_available'      => (isset($available) && $available == 'on') ? 1 : 0,
+        'position'          => (!empty($position) ? $position : 0),
+        'item_variety_id'   => (!empty($variety_id) ? $variety_id : 0),
+        'name'              => (!empty($name) ? $name : NULL),
+        'is_available'      => (isset($is_available) && $is_available == 'on') ? 1 : 0,
+        'is_wholesale'      => (isset($is_wholesale) && $is_wholesale == 'on') ? 1 : 0,
+        'price'             => $price * 100,
+        'quantity'          => (isset($quantity)) ? $quantity : 0,
+        'package_type_id'   => $package_type,
+        'measurement'       => (!empty($measurement) && !empty($metric)) ? $measurement : 0,
+        'metric_id'         => (!empty($measurement) && !empty($metric)) ? $metric : 0,
     ]);
     
     if (!$updated) {
