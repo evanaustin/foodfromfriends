08d1118bacdd25c2fe3021b3887f21843b9a2125
EvanGrinde <evan.grinde@gmail.com>
2018-06-20 09:27:31 -0400

    check for similar items in add new initializer

--- routes/initializers/dashboard/selling/items/add-new.php
+++ routes/initializers/dashboard/selling/items/add-new.php
@@ -4,8 +4,9 @@ $settings = [
     'title' => 'Add a new item | Food From Friends'
 ];
 
-$category_id    = \Num::clean_int($_GET['category']);
-$subcategory_id = \Num::clean_int($_GET['subcategory']);
+$category_id    = (isset($_GET['category']))    ? \Num::clean_int($_GET['category']) : 0;
+$subcategory_id = (isset($_GET['subcategory'])) ? \Num::clean_int($_GET['subcategory']) : 0;
+$variety_id     = (isset($_GET['variety']))     ? \Num::clean_int($_GET['variety']) : 0;
 
 $Item = new Item([
     'DB' => $DB
@@ -43,11 +44,17 @@ $metrics        = $Item->retrieve([
     'table' => 'item_metrics'
 ]);
 
+$where_similar = [
+    'grower_operation_id'   => $User->GrowerOperation->id,
+    'item_subcategory_id'   => $subcategory_id
+];
+
+if (!empty($variety_id)) {
+    $where_similar['item_variety_id'] = $variety_id;
+}
+
 $similar_items  = $Item->retrieve([
-    'where' => [
-        'grower_operation_id'   => $User->GrowerOperation->id,
-        'item_subcategory_id'   => $subcategory_id
-    ]
+    'where' => $where_similar
 ]);
 
 $item_images    = [];
