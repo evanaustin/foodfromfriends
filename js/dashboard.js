04397c620fb6a7e748d7bd8f4574457a60b82f46
EvanGrinde <evan.grinde@gmail.com>
2018-06-20 09:22:22 -0400

    enable keyboard shortcuts on dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,6 +3,22 @@ App.Dashboard = function() {
         $(document).on('shown.bs.modal', '.bootbox.modal', function() {
             $(this).find('.modal-header').append("<button type='button' class='bootbox-close-button close' data-dismiss='modal'>&times;</button>");
         });
+
+        // Enable keyboard shortcuts
+        $(document).keyup(function(e) {
+            if (!($('input, textarea, select').is(':focus'))) {
+                switch(e.keyCode) {
+                    case 69: // e
+                        $('#sidebar-exchange-options').trigger('click');
+                        break;
+                    case 73: // i
+                        $('#sidebar-items').trigger('click');
+                        break;
+                    case 79: // o
+                        $('#sidebar-orders').trigger('click');
+                }
+            }
+        });
     };
 
     return {
