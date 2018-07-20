7f474c54fe32880108aaa3552a1923dece0be6d7
EvanGrinde <evan.grinde@gmail.com>
2018-07-20 16:57:55 -0400

    shoe meetup deadline and order minimum on exchange option modal

--- routes/modals/seller-profile.php
+++ routes/modals/seller-profile.php
@@ -4,7 +4,7 @@
             <div class="modal-header">
                 <h3 class="modal-title">
                     <!-- <span id="zoom-title"></span> -->
-                    <?= $Seller->name; ?>
+                    <?= $SellerAccount->name; ?>
                 </h3>
 
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -15,7 +15,7 @@
             <div class="modal-body">
                 <!-- <div id="zoom-src"></span> -->
                 
-                <?= _img(ENV . '/grower-operation-images/' . $Seller->filename, $Seller->ext, [
+                <?= _img(ENV . '/grower-operation-images/' . $SellerAccount->filename, $SellerAccount->ext, [
                     'server'    => 'S3',
                     'class'     => 'img-fluid rounded drop-shadow'
                 ]); ?>
@@ -28,10 +28,14 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title">Exchange option</h3>
+                <h3 class="modal-title">
+                    Exchange option
+                </h3>
 
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span> 
+                    <span aria-hidden="true">
+                        &times;
+                    </span> 
                 </button>
             </div>
             
@@ -40,9 +44,9 @@
 
                 <form id="set-exchange-option">
                         
-                    <?php if ($Seller->Delivery && $Seller->Delivery->is_offered): ?>
+                    <?php if ($SellerAccount->Delivery && $SellerAccount->Delivery->is_offered): ?>
 
-                        <?php $in_range = (isset($distance_miles) && $distance_miles <= $Seller->Delivery->distance); ?>
+                        <?php $in_range = (isset($distance_miles) && $distance_miles <= $SellerAccount->Delivery->distance); ?>
                         
                         <div class="exchange callout bubble <?php if (!$in_range) echo 'danger disabled' ?>" data-exchange="delivery">
                             <div class="muted font-18 thick">
@@ -50,7 +54,7 @@
                             </div>
                             
                             <div>
-                                Will deliver within: <?= $Seller->Delivery->distance ?> miles 
+                                Will deliver within: <?= $SellerAccount->Delivery->distance ?> miles 
 
                                 <?php if (!$in_range && isset($distance_miles)): ?>
 
@@ -60,10 +64,10 @@
 
                             </div>
 
-                            <?php if ($Seller->Delivery->delivery_type == 'conditional'): ?>
+                            <?php if ($SellerAccount->Delivery->delivery_type == 'conditional'): ?>
                                 
                                 <div>
-                                    Free delivery within: <?= $Seller->Delivery->free_distance ?> miles
+                                    Free delivery within: <?= $SellerAccount->Delivery->free_distance ?> miles
                                 </div>
 
                             <?php endif; ?>
@@ -75,7 +79,7 @@
                                 </div>
 
                                 <div>
-                                    Cost: <?= ($Seller->Delivery->delivery_type == 'free' ? 'Free' : 'Rate: $' . number_format($Seller->Delivery->fee / 100, 2) . ' ' . str_replace('-', ' ', $Seller->Delivery->pricing_rate)); ?>
+                                    Cost: <?= ($SellerAccount->Delivery->delivery_type == 'free' ? 'Free' : 'Rate: $' . number_format($SellerAccount->Delivery->fee / 100, 2) . ' ' . str_replace('-', ' ', $SellerAccount->Delivery->pricing_rate)); ?>
                                 </div>
 
                             <?php endif; ?>
@@ -84,7 +88,7 @@
 
                     <?php endif; ?>
 
-                    <?php /* if ($Seller->Meetup && $Seller->Meetup->is_offered): ?>
+                    <?php /* if ($SellerAccount->Meetup && $SellerAccount->Meetup->is_offered): ?>
                         
                         <div class="exchange callout bubble" data-exchange-option="meetup">
                             <div class="muted font-18 thick">
@@ -92,9 +96,9 @@
                             </div>
 
                             <div>
-                                <?= $Seller->Meetup->address_line_1 . (($Seller->Meetup->address_line_2) ? ", {$Seller->Meetup->address_line_2}" : '') ?><br>
-                                <?= "{$Seller->Meetup->city}, {$Seller->Meetup->state} {$Seller->Meetup->zipcode}" ?><br>
-                                <?= $Seller->Meetup->time ?>
+                                <?= $SellerAccount->Meetup->address_line_1 . (($SellerAccount->Meetup->address_line_2) ? ", {$SellerAccount->Meetup->address_line_2}" : '') ?><br>
+                                <?= "{$SellerAccount->Meetup->city}, {$SellerAccount->Meetup->state} {$SellerAccount->Meetup->zipcode}" ?><br>
+                                <?= $SellerAccount->Meetup->time ?>
                             </div>
                         </div>
 
@@ -104,7 +108,7 @@
                             
                         <?php foreach ($meetups as $meetup): ?>
                         
-                            <div class="exchange callout bubble" data-exchange="<?= $meetup['id'] ?>">
+                            <div class="exchange callout bubble <?php if (!empty($meetup['deadline']) || !empty($meetup['order_minimum'])) echo 'warning text-muted' ?>" data-exchange="<?= $meetup['id'] ?>">
                                 <div class="muted font-18 thick">
                                     <i class="fa fa-map-signs" data-toggle="tooltip" data-title="Meet here at the time specified to pick up your order"></i>
                                     <?= (!empty($meetup['title'])) ? $meetup['title'] : "{$meetup['address_line_1']} {$meetup['address_line_2']}" ?>
@@ -121,6 +125,26 @@
                                 <div>
                                     <?= "{$meetup['day']} &bull; {$meetup['start_time']} &ndash; {$meetup['end_time']}" ?>
                                 </div>
+
+                                <?php if (!empty($meetup['deadline'])): ?>
+                                        
+                                    <div>
+                                        Order
+                                        <strong class="warning"><?= $meetup['deadline'] ?></strong>
+                                        hours in advance
+                                    </div>
+                                
+                                <?php endif ?>
+
+                                <?php if (!empty($meetup['order_minimum'])): ?>
+                        
+                                    <div>
+                                        <strong class="warning"><?= _amount($meetup['order_minimum']) ?></strong>
+                                        minimum order
+                                    </div>
+                                
+                                <?php endif ?>
+
                             </div>
                     
                         <?php endforeach ?>
