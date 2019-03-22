b7b2c231013d60f19aa514a26c2ad34a01979bb2
EvanGrinde <evan.grinde@gmail.com>
2019-03-22 11:56:50 -0400

    fix cols bhg

--- routes/views/bighousefarm.php
+++ routes/views/bighousefarm.php
@@ -28,12 +28,12 @@
             <div class="sidekick">
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-12 col-md-8">
+                        <div class="col-12 col-md-8">
                             <p>We are a 5th generation farm that earns trust through transparent farming by producing nutrient-rich, pasture raised, fed organic feed poultry.</p>
                             <p>Our mission is to earn and maintain the trust of our community through transparent farming, which nurtures the soil and grasses to produce nutrient rich food for our family, friends and patrons.</p>
                         </div>
 
-                        <div class="col-sm-12 col-md-4">
+                        <div class="col-12 col-md-4">
 
                             <?= _img('../media/bighousefarm/log-grain','png',[
                                 'server'    => 'local',
@@ -48,11 +48,11 @@
             <div class="sidekick">
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-12 col-md-8 order-md-2">
+                        <div class="col-12 col-md-8 order-md-2">
                             <p>Our chickens are never medicated, never fed a GMO grain, and never mistreated. They live on grass with plenty of room to move around and forage for bugs in open air chicken tractors that are moved daily to fresh ground.</p>
                         </div>
                         
-                        <div class="col-sm-12 col-md-4 order-md-1">
+                        <div class="col-12 col-md-4 order-md-1">
 
                             <?= _img('../media/bighousefarm/chicken','png',[
                                 'server'    => 'local',
@@ -76,16 +76,16 @@
                         </div>
 
                         <div class="row">
-                            <div class="col-md-4 offset-md-4">
+                            <div class="col-12 col-md-4 offset-md-4">
                                 <input type="hidden" name="grower-operation-id" value="55"/>
                                 <input type="text" name="email" class="form-control" placeholder="Enter your email"/>
                                 
                                 <div class="row">
-                                    <div class="col-sm-9">
+                                    <div class="col-9">
                                         <input type="text" name="city" class="form-control" placeholder="Enter your city"/>
                                     </div>
 
-                                    <div class="col-sm-3" data-toggle="tooltip" data-title="We're available only in the state of Virginia right now">
+                                    <div class="col-3" data-toggle="tooltip" data-title="We're available only in the state of Virginia right now">
                                         <input type="text" name="city" class="form-control" value="VA" disabled>
                                     </div>
                                 </div>
